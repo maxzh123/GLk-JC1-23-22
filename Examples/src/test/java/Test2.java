a81b40f72bfff41151a3736dd2abb1dd5df5cfdb
Maksim S Zhalezny <M.Geleznu@novacom.by>
2022-02-22 11:56:58 +0300

    Чистка теста

--- Examples/src/test/java/Test2.java
+++ Examples/src/test/java/Test2.java
@@ -8,25 +8,6 @@ public class Test2 {
 
     @Test
     public void test1() {
-        Task1.main(null);
-        Task2[] array2 = new Task2[10];
-        for (int i = 0; i < array2.length; i++) {
-            array2[i] = new Task2();
-        }
-        // ...........
-        for (int i = 0; i < array2.length; i++) {
-            array2[i].add(i);
-        }
-        // ...........
-        for (int i = 0; i < array2.length; i++) {
-            System.out.print(i + ":" + "->");
-            array2[i].print();
-        }
-        System.out.println("Сортируем");
-        Arrays.sort(array2);
-        for(Task2 t:array2){
-            System.out.println(t);
-        }
 
     }
 
