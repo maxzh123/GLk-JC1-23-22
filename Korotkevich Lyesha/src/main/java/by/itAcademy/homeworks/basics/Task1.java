b9e2fbd499c8a345b9105ffa7dd11a7ef9bb0576
LyeshaKorotkevich <100153546+LyeshaKorotkevich@users.noreply.github.com>
2022-02-22 19:19:15 +0300

    Update Task1.java

--- Korotkevich Lyesha/src/main/java/by/itAcademy/homeworks/basics/Task1.java
+++ Korotkevich Lyesha/src/main/java/by/itAcademy/homeworks/basics/Task1.java
@@ -2,6 +2,6 @@ package by.itAcademy.homeworks.basics;
 
 public class Task1 {
   public static void main(String[] args){
-    System.out.print("Hello World!!!")
+    System.out.print("Hello World!!!");
   }
 }
