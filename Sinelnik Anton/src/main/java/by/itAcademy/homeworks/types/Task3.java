e382eab32475b64542e3353ee64ccf0c47baacd7
anton <anton@example.com>
2022-02-23 09:53:03 +0300

    "второе третье задание" done

--- Sinelnik Anton/src/main/java/by/itAcademy/homeworks/types/Task3.java
+++ Sinelnik Anton/src/main/java/by/itAcademy/homeworks/types/Task3.java
@@ -1,4 +1,15 @@
 package by.itAcademy.homeworks.types;
 
 public class Task3 {
+    public static void main(String[] args) {
+        byte b = 0x55;
+        short s = 0x55ff;
+        int i = 1000000;
+        long l = 0xffffffffL;
+        char c = 'a' ;
+        float f = .25f;
+        double d = .00001234;
+        boolean bool = true;
+        System.out.print("значения переменных равны:"+b +", "+s +", "+i +", " +l +", "+c +", "+f +", " +d +", " +bool);
+    }
 }
