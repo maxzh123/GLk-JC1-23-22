156db83711b4d42489f295d4dc3d6c9f442b7b1c
Vlad Arkhangelskiy <vladarkhangelskiy>
2022-02-23 22:55:21 +0300

    task 3 (не смог инициализировать byte)

--- Arkhangelskiy Vlad/src/main/java/by/itAcademy/homeworks/types/Task3.java
+++ Arkhangelskiy Vlad/src/main/java/by/itAcademy/homeworks/types/Task3.java
@@ -1,4 +1,23 @@
 package by.itAcademy.homeworks.types;
 
 public class Task3 {
+
+    public static void main(String[] args) {
+        short s = 0x55ff;
+        int i = 1000000;
+        long l = 0xffffffffL;
+        char c = 'a';
+        float f = .25f;
+        double d = .00001234;
+        boolean bool = true;
+
+        System.out.println(s);
+        System.out.println(i);
+        System.out.println(l);
+        System.out.println(c);
+        System.out.println(f);
+        System.out.println(d);
+        System.out.println(bool);
+    }
+
 }
