526341fb729eb581fd811c5d7fe1a71c463d1e8c
leha <torlesh18112003@gmail.com>
2022-02-23 17:08:30 +0300

    Метод

--- Korotkevich Lyesha/src/main/java/by/itAcademy/homeworks/types/Task3.java
+++ Korotkevich Lyesha/src/main/java/by/itAcademy/homeworks/types/Task3.java
@@ -1,4 +1,16 @@
 package by.itAcademy.homeworks.types;
+import java.util.Scanner;
 
 public class Task3 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println(mul(sc.nextInt(), sc.nextInt()));
+    }
+    public static int mul(int a, int b){
+        int s = 0;
+        s += a+b;
+        s += a*b;
+        return s;
+    }
 }
+
