a098e23195639b379c97c8496f07a4056bf985fe
Olga Melanich <beekwin@gmail.com>
2022-02-23 19:48:48 +0300

    Update task1, task2, task3

--- Melanich Olga/src/main/java/by/itAcademy/homeworks/basics/Task2.java
+++ Melanich Olga/src/main/java/by/itAcademy/homeworks/basics/Task2.java
@@ -1,4 +1,14 @@
 package by.itAcademy.homeworks.basics;
 
-public class Task2 {
+import by.itAcademy.interfaces.basics.ITask2;
+
+/** Задание 2
+ *
+ * Набрать Hello Belarus!, откомпилировать, запустить.
+ */
+
+public class Task2 implements ITask2 {
+    public static void main(String[] args) {
+        System.out.println("Hello Belarus!");
+    }
 }
