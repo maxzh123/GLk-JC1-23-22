96e0f399aad4a5dac1f1de62e1642bf11ff95fe2
Vlad Arkhangelskiy <vladarkhangelskiy>
2022-02-23 22:58:50 +0300

    task 3

--- Arkhangelskiy Vlad/src/main/java/by/itAcademy/homeworks/types/Task3.java
+++ Arkhangelskiy Vlad/src/main/java/by/itAcademy/homeworks/types/Task3.java
@@ -3,6 +3,7 @@ package by.itAcademy.homeworks.types;
 public class Task3 {
 
     public static void main(String[] args) {
+        byte b = 0x55;
         short s = 0x55ff;
         int i = 1000000;
         long l = 0xffffffffL;
@@ -11,6 +12,7 @@ public class Task3 {
         double d = .00001234;
         boolean bool = true;
 
+        System.out.println(b);
         System.out.println(s);
         System.out.println(i);
         System.out.println(l);
