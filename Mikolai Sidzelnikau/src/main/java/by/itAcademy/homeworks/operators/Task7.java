5b42564ba64c77963317706b729b20358b43a452
Mikolai Sidzelnikau <mikolakran@example.com>
2022-02-24 23:32:36 +0300

    operators Task7 можно ли закрыть круглой картонкой прямоугольник

--- Mikolai Sidzelnikau/src/main/java/by/itAcademy/homeworks/operators/Task7.java
+++ Mikolai Sidzelnikau/src/main/java/by/itAcademy/homeworks/operators/Task7.java
@@ -3,21 +3,22 @@ package by.itAcademy.homeworks.operators;
 /**
  * ## Задание 7
  * > Имеется прямоугольное отверстие размерами a и b, где a и b – целые числа.
- > Определить, можно ли его полностью закрыть круглой картонкой радиусом r (тоже
- > целое число).*/
+ * > Определить, можно ли его полностью закрыть круглой картонкой радиусом r (тоже
+ * > целое число).
+ */
 
 public class Task7 {
     public static void main(String[] args) {
         int a = 5;
         int b = 12;
-        int p = a*b; //площадь прямоугольника = 60
+        int p = a * b; //площадь прямоугольника = 60
         int r = 6;
-        double pr = Math.PI *(r*r); // площадь круга = 113
+        double pr = Math.PI * (r * r); // площадь круга = 113
         boolean b1;
-        if (pr<p){
+        if (pr < p) {
             b1 = false;
             System.out.println(b1);
-        }else {
+        } else {
             b1 = true;
             System.out.println(b1);   // ответ true
         }
