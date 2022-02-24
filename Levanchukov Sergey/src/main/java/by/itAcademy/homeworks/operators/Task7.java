7f577a26213d39e0a6d38c0e0c290b795ca16f0c
IT-Academy-Gomel <IT-Academy-Gomel>
2022-02-24 21:40:57 +0300

    Примеры

--- Levanchukov Sergey/src/main/java/by/itAcademy/homeworks/operators/Task7.java
+++ Levanchukov Sergey/src/main/java/by/itAcademy/homeworks/operators/Task7.java
@@ -11,6 +11,10 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Task7 {
+    private static class Circle{
+        int a,b,r;
+    }
+
     static void Circle (int a,int b,int r){
         if (r*2>=Math.sqrt((Math.pow(a,2)+Math.pow(b,2)))){
             System.out.println("Картонка радиусом "+r+" закрывает прямоугольное отверстие со сторонами a="+a+" b="+b+".");
@@ -19,13 +23,15 @@ public class Task7 {
             System.out.println("Картонка не закрывает прямоугольное отверстие");
         }
     }
-    /*static void Value() throws IOException {
+    static Circle value() throws IOException {
 
         BufferedReader reader=new BufferedReader(new InputStreamReader(System.in));
-        int a=Integer.parseInt(reader.readLine());
-        int b=Integer.parseInt(reader.readLine());
-        int r=Integer.parseInt(reader.readLine());
-    }*/
+        Circle result=new Circle();
+        result.a=Integer.parseInt(reader.readLine());
+        result.b=Integer.parseInt(reader.readLine());
+        result.r=Integer.parseInt(reader.readLine());
+        return result;
+    }
     public static void main(String[] args) throws IOException {
         BufferedReader reader=new BufferedReader(new InputStreamReader(System.in));
         int a=Integer.parseInt(reader.readLine());
