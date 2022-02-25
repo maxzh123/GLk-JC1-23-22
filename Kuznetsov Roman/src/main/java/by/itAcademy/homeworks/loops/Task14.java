67f45b2d36bfa40f7986f2d1d3c4781637fac747
Roman Kuznetsov <mmamano@inbox.ru>
2022-02-25 16:11:57 +0300

    Completed "Loops" tasks

--- Kuznetsov Roman/src/main/java/by/itAcademy/homeworks/loops/Task14.java
+++ Kuznetsov Roman/src/main/java/by/itAcademy/homeworks/loops/Task14.java
@@ -1,4 +1,20 @@
 package by.itAcademy.homeworks.loops;
 
+import java.util.Scanner;
+
+/**
+ * Используя цикл do-while суммировать все цифры числа.
+ *
+ */
 public class Task14 {
+    public static void main(String[] args) {
+        int i=0; int result = 0;
+        Scanner scanner = new Scanner(System.in);
+        char[] number = scanner.nextLine().toCharArray(); // разлаживаем нужное число в массив символов.
+        do{
+            result+=Integer.parseInt(String.valueOf(number[i])); // преобразуем один элемент char в int и суммируем
+            i++;
+        }while(i<number.length);
+        System.out.println(result);
+    }
 }
