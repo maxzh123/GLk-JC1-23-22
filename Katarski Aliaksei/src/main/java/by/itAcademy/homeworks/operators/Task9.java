1bae639ae7ec2e3402f88e2007cad0c0690bdcb3
Katarski Aliaksei <Katarski Aliaksei>
2022-02-25 19:23:34 +0300

    Done

--- Katarski Aliaksei/src/main/java/by/itAcademy/homeworks/operators/Task9.java
+++ Katarski Aliaksei/src/main/java/by/itAcademy/homeworks/operators/Task9.java
@@ -1,4 +1,15 @@
 package by.itAcademy.homeworks.operators;
+/**> Задать три числа – день, месяц, год. Вывести на экран в виде трех чисел дату
+ > следующего дня.
+*/
 
 public class Task9 {
+    public static void main(String[] args){
+        int day=13;
+        int month=3;
+        int year=2022;
+        day++;
+        System.out.print(day+" "+month+" "+year);
+    }
+
 }
