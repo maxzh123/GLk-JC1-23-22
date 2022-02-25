4884e096c4774edecfbe6da82d383f86f2b5da05
Katarski Aliaksei <Katarski Aliaksei>
2022-02-25 20:01:42 +0300

    12 task

--- Katarski Aliaksei/src/main/java/by/itAcademy/homeworks/loops/Task12.java
+++ Katarski Aliaksei/src/main/java/by/itAcademy/homeworks/loops/Task12.java
@@ -1,4 +1,19 @@
 package by.itAcademy.homeworks.loops;
+/**Вычислить факториал целых чисел от 0 до 10 с помощью цикла while
+*/
 
 public class Task12 {
+    public static void main(String[] args){
+        int firstLoop=0;
+        int i=0;
+        while(i<=10){
+            if(firstLoop<=0){
+                firstLoop++;
+            }
+            firstLoop*=i;
+            i++;
+            System.out.println(firstLoop);
+        }
+    }
 }
+//Не знаю как избежать костыля с If.
