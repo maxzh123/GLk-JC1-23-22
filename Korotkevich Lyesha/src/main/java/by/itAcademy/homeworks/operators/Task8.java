a4c72c3d9f213a8e998eeb29e423caae146a6eeb
leha <torlesh18112003@gmail.com>
2022-02-27 19:19:20 +0300

    redacted task 8

--- Korotkevich Lyesha/src/main/java/by/itAcademy/homeworks/operators/Task8.java
+++ Korotkevich Lyesha/src/main/java/by/itAcademy/homeworks/operators/Task8.java
@@ -8,7 +8,7 @@ public class Task8 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int sum = sc.nextInt();
-        if (sum %10 == 1){
+        if (sum %10 == 1 && sum != 11){
             System.out.println(sum + " рубль");
         } else if (sum % 10 == 2 || sum % 10 == 3 || sum % 10 == 4 ){
             System.out.println(sum + " рубля");
