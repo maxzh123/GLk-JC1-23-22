f419c322d1dfc9829125faa18020307d93fe2627
Леванчуков Сергей <leva@example.com>
2022-02-27 21:00:38 +0300

    Update Task 7 Done 10-22

--- Levanchukov Sergey/src/main/java/by/itAcademy/homeworks/loops/Task12.java
+++ Levanchukov Sergey/src/main/java/by/itAcademy/homeworks/loops/Task12.java
@@ -1,4 +1,12 @@
 package by.itAcademy.homeworks.loops;
-
+/**Вычислить факториал целых чисел от 0 до 10 с помощью цикла while*/
 public class Task12 {
+    public static void main(String[] args) {
+        int i=1,result=1;
+        while (i<10){
+            result*=i;
+            i++;
+        }
+        System.out.println("Факториал 10 равен "+ result);
+    }
 }
