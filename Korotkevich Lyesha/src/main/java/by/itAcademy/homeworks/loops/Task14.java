9fcf20b2724a464d1e213459d376130bdf5b6645
leha <torlesh18112003@gmail.com>
2022-02-28 21:04:15 +0300

    Task 14

--- Korotkevich Lyesha/src/main/java/by/itAcademy/homeworks/loops/Task14.java
+++ Korotkevich Lyesha/src/main/java/by/itAcademy/homeworks/loops/Task14.java
@@ -1,4 +1,15 @@
 package by.itAcademy.homeworks.loops;
 
 public class Task14 {
+    public static void main(String[] args){
+        System.out.println(digital_sum(7893823445L));
+    }
+    public static long digital_sum(long num){
+        long sum = 0;
+        while(num > 10){
+            sum += num %10;
+            num /= 10;
+        }
+        return (sum+num);
+    }
 }
