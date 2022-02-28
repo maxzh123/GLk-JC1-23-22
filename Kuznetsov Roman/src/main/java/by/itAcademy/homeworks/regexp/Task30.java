f396f6073711717f10c7e089b7428f872069fe2f
Roman Kuznetsov <mmamano@inbox.ru>
2022-02-28 14:54:21 +0300

    Completed "regex" tasks and improved isPrime task

--- Kuznetsov Roman/src/main/java/by/itAcademy/homeworks/regexp/Task30.java
+++ Kuznetsov Roman/src/main/java/by/itAcademy/homeworks/regexp/Task30.java
@@ -1,4 +1,16 @@
 package by.itAcademy.homeworks.regexp;
 
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Task30 {
+    public static String htmlEdit(String text){
+        return text.replaceAll("<p\\s[^>]*>","<p>");
+    }
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String text = scanner.nextLine();
+        System.out.println(htmlEdit(text));
+    }
 }
