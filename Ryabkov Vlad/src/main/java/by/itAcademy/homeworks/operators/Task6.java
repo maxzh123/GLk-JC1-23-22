c9f1e096723f19c2a7e247be999c6607ecc655f7
Vlad <vladryabkov1998@gmail.com>
2022-02-28 12:38:32 +0300

    Не совсем уверен, что я сделал правильно. Но по-другому сделать пока не могу)

--- Ryabkov Vlad/src/main/java/by/itAcademy/homeworks/operators/Task6.java
+++ Ryabkov Vlad/src/main/java/by/itAcademy/homeworks/operators/Task6.java
@@ -1,4 +1,20 @@
 package by.itAcademy.homeworks.operators;
 
+/**
+ * ## Задание 6
+ * > Создайте метод с одним целочисленным параметром. Метод должен
+ * > определить, является ли последняя цифра числа семеркой и вернуть boolean
+ * > значение.
+ */
+
 public class Task6 {
+    public static void main(String[] args) {
+        int value = 5477;
+        int check = value % 10;
+        if (check == 7){
+            System.out.println(true);
+        }else
+            System.out.println(false);
+
+    }
 }
