aac46b416ccc9e5b44198b168ccf6b8134f12ec1
Katarski Aliaksei <Katarski Aliaksei>
2022-03-01 19:08:35 +0300

    task 20 done

--- Katarski Aliaksei/src/main/java/by/itAcademy/homeworks/arrays/Task20.java
+++ Katarski Aliaksei/src/main/java/by/itAcademy/homeworks/arrays/Task20.java
@@ -1,4 +1,29 @@
 package by.itAcademy.homeworks.arrays;
 
+import java.util.Arrays;
+
+/**
+ * Создать массив ,найти максимальное значение и вывести индекс этого элемента.
+ */
 public class Task20 {
+    public static void main(String[] args) {
+        int[] array = new int[10];
+        fillArray(array);
+        maxEl(array);
+    }
+
+    public static void fillArray(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            arr[i]=(int)(Math.random()*10);
+        }
+
+    }
+    public static void maxEl(int[] arr){
+        int max=arr[0];
+        for(int i=0;i<arr.length;i++){
+           if(max<arr[i]){
+               max=arr[i];
+           }
+        }System.out.println(max);
+    }
 }
