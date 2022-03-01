6545c51c52545ebf9483218e2421257491255ac2
Roman Kuznetsov <mmamano@inbox.ru>
2022-03-01 13:10:06 +0300

    Completed OOP and enum tasks

--- Kuznetsov Roman/src/main/java/by/itAcademy/homeworks/oop/Task32.java
+++ Kuznetsov Roman/src/main/java/by/itAcademy/homeworks/oop/Task32.java
@@ -1,4 +1,51 @@
 package by.itAcademy.homeworks.oop;
 
+import java.util.Date;
+
+class Appliance {
+    protected String producer;
+    protected double weigth;
+    protected Date prodtime;
+    protected String serialnumber;
+    public String isTurnedOn(){
+        return "Makes some action";
+    }
+    public Appliance(String producer, double weigth, Date prodtime, String serialnumber) {
+        this.producer = producer;
+        this.weigth = weigth;
+        this.prodtime = prodtime;
+        this.serialnumber = serialnumber;
+    }
+}
+class CoolingAppliance extends Appliance {
+    protected double electricityRequired;
+    @Override
+    public String isTurnedOn() {
+        return "Freezes";
+    }
+    public CoolingAppliance(String producer, double weigth, Date prodtime, String serialnumber, double electricityRequire) {
+        super(producer, weigth, prodtime, serialnumber);
+        this.electricityRequired = electricityRequire;
+    }
+}
+class Fridge extends CoolingAppliance {
+    protected String formfactor;
+    @Override
+    public String isTurnedOn() {
+        return "Protects food";
+    }
+    public Fridge(String producer, double weigth, Date prodtime, String serialnumber, double electricityRequire, String formfactor) {
+        super(producer, weigth, prodtime, serialnumber, electricityRequire);
+        this.formfactor = formfactor;
+    }
+}
 public class Task32 {
+    public static void main(String[] args) {
+        Appliance a = new Appliance("Bosh",12,new Date(),"AE123THC");
+        CoolingAppliance b = new CoolingAppliance("Bosh",12,new Date(),"AE123THC",220);
+        Fridge c = new Fridge("Bosh",12,new Date(),"AE123THC",220,"Mini");
+        System.out.println("Appliance: "+a.isTurnedOn());
+        System.out.println("CoolingApplication "+b.isTurnedOn());
+        System.out.println("Fridge "+c.isTurnedOn());
+    }
 }
