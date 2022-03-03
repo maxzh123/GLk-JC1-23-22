fa5c267e66b886d6465e2295bcac030e305e912f
gamezo90 <gamezo90@mail.ru>
2022-03-03 15:30:06 +0300

    types -> Task6

--- Gamezo Nikolai/src/main/java/by/itAcademy/homeworks/operators/Task6.java
+++ Gamezo Nikolai/src/main/java/by/itAcademy/homeworks/operators/Task6.java
@@ -1,4 +1,17 @@
 package by.itAcademy.homeworks.operators;
 
+/**
+    Создайте метод с одним целочисленным параметром. Метод должен определить, является ли последняя цифра числа семеркой и вернуть boolean значение.
+ */
+
 public class Task6 {
+    public static void main(String[] args) {
+        int i = 101;
+        boolean b=true;
+        if (i % 10 == 7) {
+            b=true;
+        } else
+            b=false;
+        System.out.println(b);
+    }
 }
