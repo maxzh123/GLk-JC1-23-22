1c66f3440e9c22438d26f3872ffb6248d0374e18
Mikolai Sidzelnikau <mikolakran@example.com>
2022-03-03 17:08:41 +0300

    types Task4 исправил

--- Mikolai Sidzelnikau/src/main/java/by/itAcademy/homeworks/types/Task4.java
+++ Mikolai Sidzelnikau/src/main/java/by/itAcademy/homeworks/types/Task4.java
@@ -1,15 +1,16 @@
 package by.itAcademy.homeworks.types;
 import java.util.Scanner;
-
+/**## Задание 4
+        > Написать метод, который принимает на вход два целых числа, делает их
+        > суммирование и складывает результат с произведением двух этих чисел, и
+        > возвращает полученный результат из метода. Передать на вход в метод любые два
+        > числа. Вывести полученный результат работы метода на экран*/
 public class Task4 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int s = scanner.nextInt();
         int r = scanner.nextInt();
-        int sum = s*r;
-        int sum1 = sum + s;
-        int sum2 = sum + r;
+        int sum1 = (s + r) + (s*r);
         System.out.println(sum1);
-        System.out.println(sum2);
     }
 }
