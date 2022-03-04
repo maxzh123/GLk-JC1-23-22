e5e81e33b04874165f209c9b1e8a54ab329c4105
Mikolai Sidzelnikau <mikolakran@example.com>
2022-03-04 23:42:07 +0300

    types Task4 исправил

--- Mikolai Sidzelnikau/src/main/java/by/itAcademy/homeworks/types/Task4.java
+++ Mikolai Sidzelnikau/src/main/java/by/itAcademy/homeworks/types/Task4.java
@@ -6,11 +6,18 @@ import java.util.Scanner;
         > возвращает полученный результат из метода. Передать на вход в метод любые два
         > числа. Вывести полученный результат работы метода на экран*/
 public class Task4 {
+    public static int sumScanner;
+    public static int sumScanner2;
+    public static int k = 0;
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int s = scanner.nextInt();
-        int r = scanner.nextInt();
-        int sum1 = (s + r) + (s*r);
-        System.out.println(sum1);
+         sumScanner2 = scanner.nextInt();
+         sumScanner = scanner.nextInt();
+        System.out.println(sumirovanie(sumScanner, sumScanner2));
+
+    }
+    public static int sumirovanie(int sum2, int sum3){
+         k = (sum2 + sum3) + (sum2*sum3);
+        return k;
     }
 }
