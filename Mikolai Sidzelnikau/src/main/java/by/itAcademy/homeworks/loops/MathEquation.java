ccc8fdf6e3c07eef948dacd18b60479444ee55f5
Mikolai Sidzelnikau <mikolakran@example.com>
2022-03-04 23:21:20 +0300

    loops Task15 Нахождение простых чисел

--- Mikolai Sidzelnikau/src/main/java/by/itAcademy/homeworks/loops/MathEquation.java
+++ Mikolai Sidzelnikau/src/main/java/by/itAcademy/homeworks/loops/MathEquation.java
@@ -1,6 +1,42 @@
 package by.itAcademy.homeworks.loops;
 
 public class MathEquation {
+    /**
+     * Нахождение простых чисел от 2 до 2147483647
+     */
+    public static int primeNumber(int nuumber, int number2) {
+        while (nuumber < number2) {
+            int r = 2;
+            boolean im = true;
+            while (r < nuumber) {
+                if (nuumber % r == 0) {
+                    im = false;
+                    break;
+                }
+                r++;
+            }
+            if (im) {
+                System.out.println(nuumber);
+            }
+            nuumber++;
+        }
+        return nuumber;
+    }
+
+
+    /**
+     * Заполнение масива числами
+     */
+    public static int s = 0;
+
+    public static int[] intas(int[] suma) {
+        while (s < suma.length) {
+            suma[s] = s;
+            //       System.out.println(suma[s]);
+            s++;
+        }
+        return suma;
+    }
 
     /**
      * факториал числа произведение чисел
