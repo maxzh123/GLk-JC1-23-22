47c035c7cd35e79776c0aeb7d800976bd07b3cf0
Roman Kuznetsov <mmamano@inbox.ru>
2022-03-05 10:35:41 +0300

    Completed task 38

--- Kuznetsov Roman/src/main/java/by/itAcademy/homeworks/generics/Task38.java
+++ Kuznetsov Roman/src/main/java/by/itAcademy/homeworks/generics/Task38.java
@@ -1,4 +1,20 @@
 package by.itAcademy.homeworks.generics;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Task38 {
+    public static void main(String[] args) {
+        Car car = new Car("Bebra-1000",4,"BebroProd");
+        Car car2 = new Car("Bebra-3000",6,"Gachi");
+        Motorcycle motorcycle = new Motorcycle("R1",2,"Yamaha");
+        Motorcycle motorcycle2 = new Motorcycle("R1337",2,"Yamaha");
+        Garage<Vehicle> garage = new Garage(car,car2,motorcycle,motorcycle2);
+        System.out.println("Before adding vehicle");
+        System.out.println(garage.getAllVehicles());
+        System.out.println("After adding vehicle");
+        garage.addVehicle(motorcycle);
+        garage.addVehicle(motorcycle2);
+        System.out.println(garage.getAllVehicles());
+    }
 }
