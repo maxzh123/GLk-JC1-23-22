c645ba40a671a7260e3d5d95b1d70b4e126667d6
Roman Kuznetsov <mmamano@inbox.ru>
2022-03-05 11:44:10 +0300

    Minor fixes

--- Kuznetsov Roman/src/main/java/by/itAcademy/homeworks/collections/Task42.java
+++ Kuznetsov Roman/src/main/java/by/itAcademy/homeworks/collections/Task42.java
@@ -10,13 +10,8 @@ public class Task42 {
     }
     public static Map<String,Integer> getMapFromList(List<String> arrayList){
         Map<String,Integer> map = new LinkedHashMap<>();
-        String word;
-        int number;
-        ListIterator<String> it = arrayList.listIterator();
-        while(it.hasNext()){
-            word = it.next();
-            number = Collections.frequency(arrayList,word);
-            map.put(word, number);
+        for (String s : arrayList) {
+            map.put(s, Collections.frequency(arrayList, s));
         }
         return map;
     }
