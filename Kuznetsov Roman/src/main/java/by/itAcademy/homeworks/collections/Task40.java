7cb1b1f390975e01dfe7ae1d4c341315627f9d28
Roman Kuznetsov <mmamano@inbox.ru>
2022-03-05 11:42:16 +0300

    Completed "Collection" tasks

--- Kuznetsov Roman/src/main/java/by/itAcademy/homeworks/collections/Task40.java
+++ Kuznetsov Roman/src/main/java/by/itAcademy/homeworks/collections/Task40.java
@@ -1,4 +1,13 @@
 package by.itAcademy.homeworks.collections;
 
+import java.util.*;
+
 public class Task40 {
+    public static void main(String[] args) {
+        final int range =30;
+        List<Integer> list = (List<Integer>) Task39.fillList(new ArrayList<>(),range,100);
+        HashSet<Integer> hashSet = new HashSet(list);
+        System.out.println(list);
+        System.out.println(hashSet);
+    }
 }
--- Kuznetsov Roman/src/main/java/by/itAcademy/homeworks/collections/Task42.java
+++ Kuznetsov Roman/src/main/java/by/itAcademy/homeworks/collections/Task42.java
@@ -1,4 +1,23 @@
 package by.itAcademy.homeworks.collections;
 
+import java.util.*;
+
 public class Task42 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Map<String,Integer> map = getMapFromList(Arrays.asList(scanner.nextLine().split(" ")));
+        System.out.println(map);
+    }
+    public static Map<String,Integer> getMapFromList(List<String> arrayList){
+        Map<String,Integer> map = new LinkedHashMap<>();
+        String word;
+        int number;
+        ListIterator<String> it = arrayList.listIterator();
+        while(it.hasNext()){
+            word = it.next();
+            number = Collections.frequency(arrayList,word);
+            map.put(word, number);
+        }
+        return map;
+    }
 }
