6afbeb87bd6b7480ed9413543fdcc7aebff201c6
Aliaksandr Hameza <otulan@example.com>
2022-03-05 23:24:29 +0300

    Задания 6,7,8 выполены.

--- Hameza Aliaksandr/src/main/java/by/itAcademy/homeworks/operators/Task8.java
+++ Hameza Aliaksandr/src/main/java/by/itAcademy/homeworks/operators/Task8.java
@@ -1,4 +1,25 @@
 package by.itAcademy.homeworks.operators;
 
+/**Задать целое число в виде переменной, это число – сумма денег в рублях. Вывести это число на экран, добавив к нему слово «рублей» в правильном падеже
+ */
+
 public class Task8 {
-}
+    public static void main(String[] args) {
+        int a = 11;
+        int b = 2;
+        int sum = a + b;
+        //если число заканчивается на 1, исключение 11
+        if (sum % 10 == 1 && sum != 11) {
+            System.out.println(sum + " рубль");
+        }
+        else
+            //если число заканчивается на 2 или 3 или 4, исключение 12,13,14
+            if (sum != 12 && sum != 13 && sum != 14 && (sum % 10 == 2 || sum % 10 == 3 || sum % 10 == 4)) {
+                System.out.println(sum + " рубля");
+            }
+            //во всех остальных случаях
+            else
+                System.out.println(sum + " рублей");
+    }
+
+    }
