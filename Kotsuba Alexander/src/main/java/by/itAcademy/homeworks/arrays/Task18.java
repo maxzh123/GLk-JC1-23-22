61e58161e5124538d68aea9a4fec4cad4804cb56
Alexander8613-stack <kocuba-aleksandr@mail.ru>
2022-03-06 18:41:55 +0300

    Done

--- Kotsuba Alexander/src/main/java/by/itAcademy/homeworks/arrays/Task18.java
+++ Kotsuba Alexander/src/main/java/by/itAcademy/homeworks/arrays/Task18.java
@@ -1,4 +1,24 @@
+/*Создайте переменную для массива из 10 элементов.
+ Заполните его произвольными значениями целочисленного типа и выведите последний элемент массива на экран. */
+
 package by.itAcademy.homeworks.arrays;
 
+import java.util.Random;
+
 public class Task18 {
+    public static void main(String[] args) {
+        int array[] = new int[10];
+        Random ran = new Random();
+        for(int i = 0;i<array.length;i++){
+            array[i] = ran.nextInt(100);
+        }
+        for(int i = 0;i<array.length;i++){
+            System.out.print( " "+ array[i]);
+        }
+        System.out.println("\n Last element:  "+ lastElement(array));
+    }
+
+    public static int lastElement(int arr[]){
+        return arr[arr.length -1];
+    }
 }
