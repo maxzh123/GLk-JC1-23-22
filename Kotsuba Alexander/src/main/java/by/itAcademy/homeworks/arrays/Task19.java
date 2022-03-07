d34d7e546d3facb0c54d78829ab62a2f04b78653
Alexander8613-stack <kocuba-aleksandr@mail.ru>
2022-03-07 03:23:28 +0300

    Done

--- Kotsuba Alexander/src/main/java/by/itAcademy/homeworks/arrays/Task19.java
+++ Kotsuba Alexander/src/main/java/by/itAcademy/homeworks/arrays/Task19.java
@@ -1,4 +1,28 @@
+/*Создайте переменную для массива из 10 элементов (другим способом).
+Заполните его произвольными значениями целочисленного типа и выведите на экран элементы, стоящие на четных позициях.*/
+
 package by.itAcademy.homeworks.arrays;
 
+import java.util.Random;
+
 public class Task19 {
+    public static void main(String[] args) {
+        int array[];
+        array = new int[10];
+        Random ran = new Random();
+        for(int i = 0;i<array.length;i++){
+            array[i] = ran.nextInt(100);
+        }
+        for(int i = 0;i<array.length;i++){
+            System.out.print( " "+ array[i] );
+        }
+        System.out.println(" ");
+        even(array);
+    }
+
+    public static void even(int arr[]){
+        for(int i = 2;i<arr.length;i+=2){
+            System.out.print( " "+arr[i] );
+        }
+    }
 }
--- Kotsuba Alexander/src/main/java/by/itAcademy/homeworks/arrays/Task20.java
+++ Kotsuba Alexander/src/main/java/by/itAcademy/homeworks/arrays/Task20.java
@@ -1,4 +1,31 @@
+/*Создайте переменную для массива из 10 элементов.
+Заполните его произвольными значениями целочисленного типа. Найдите максимальный элемент и выведите его индекс.*/
+
 package by.itAcademy.homeworks.arrays;
 
+import java.util.Random;
+
 public class Task20 {
+    public static void main(String[] args) {
+        int array[] = new int[10];
+        int maxIndex = 1;
+        int max = array[0] ;
+        Random ran = new Random();
+        for(int i = 0;i<array.length;i++){
+            array[i] = ran.nextInt(100);
+        }
+        for(int i = 0;i<array.length;i++){
+            System.out.print( " "+ array[i] );
+        }
+        for (int i=0; i<array.length;i++){
+            if (array[i] > max){
+                max =array[i];
+                maxIndex = i+1;
+            }
+        }
+        System.out.print("\nИндекс элемента: "+maxIndex);
+        System.out.print("\nМаксимальный элемент массива: "+max);
+
+
+    }
 }
