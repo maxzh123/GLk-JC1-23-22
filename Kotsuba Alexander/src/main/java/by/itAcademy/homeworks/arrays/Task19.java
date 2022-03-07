e698bef3211c829a2e40040f38a311be32c66846
Alexander8613-stack <kocuba-aleksandr@mail.ru>
2022-03-07 03:26:35 +0300

    Done

--- Kotsuba Alexander/src/main/java/by/itAcademy/homeworks/arrays/Task19.java
+++ Kotsuba Alexander/src/main/java/by/itAcademy/homeworks/arrays/Task19.java
@@ -7,22 +7,22 @@ import java.util.Random;
 
 public class Task19 {
     public static void main(String[] args) {
-        int array[];
-        array = new int[10];
+        int arr[];
+        arr = new int[10];
         Random ran = new Random();
-        for(int i = 0;i<array.length;i++){
-            array[i] = ran.nextInt(100);
+        for(int i = 0;i<arr.length;i++){
+            arr[i] = ran.nextInt(100);
         }
-        for(int i = 0;i<array.length;i++){
-            System.out.print( " "+ array[i] );
+        for(int i = 0;i<arr.length;i++){
+            System.out.print( " "+ arr[i] );
         }
         System.out.println(" ");
-        even(array);
+        even(arr);
     }
 
-    public static void even(int arr[]){
-        for(int i = 2;i<arr.length;i+=2){
-            System.out.print( " "+arr[i] );
+    public static void even(int array[]){
+        for(int i = 2;i<array.length;i+=2){
+            System.out.print( " "+array[i] );
         }
     }
 }
