c4c408a025b70aa7a761f511d4ef2dbed93113d7
Aksana Barysenka <oksana_borisenko.95@example.com>
2022-03-10 00:38:00 +0300

    Done Task18-19, Done Test18-19

---
+++ Borysenka Aksana/src/test/java/arrays/Test19.java
@@ -0,0 +1,17 @@
+package arrays;
+
+
+import by.itAcademy.homeworks.arrays.Task19;
+import org.testng.annotations.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class Test19 {
+
+    @Test
+    public void Test(){
+        String expectedResult = "[1, 1, 4, 3, 7]";
+        String actualResult = Task19.evenElementArr(new int [] {1,2,1,4,4,2,3,5,7});
+        assertEquals(expectedResult, actualResult);
+    }
+}
