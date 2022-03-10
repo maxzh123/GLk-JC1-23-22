6460728d16d7899a532f00be53fb5b20ae48e781
IT-Academy-Gomel <vladryabkov1998@gmail.com>
2022-03-10 18:52:50 +0300

    _

--- Ryabkov Vlad/src/main/java/by/itAcademy/homeworks/loops/Task17.java
+++ Ryabkov Vlad/src/main/java/by/itAcademy/homeworks/loops/Task17.java
@@ -12,9 +12,6 @@ import java.util.Scanner;
 
 public class Task17 {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Введите число: ");
-        int value = scanner.nextInt();
 //        DecimalFormat formater = (DecimalFormat) NumberFormat.getInstance();
 //        DecimalFormatSymbols symbols = formater.getDecimalFormatSymbols();
 //        symbols.setGroupingSeparator(' ');
