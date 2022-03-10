58164b91e17dd0f03bd364e773b1e2e353a12842
IT-Academy-Gomel <vladryabkov1998@gmail.com>
2022-03-10 18:58:37 +0300

    _

--- Ryabkov Vlad/src/main/java/by/itAcademy/homeworks/loops/Task17.java
+++ Ryabkov Vlad/src/main/java/by/itAcademy/homeworks/loops/Task17.java
@@ -1,10 +1,5 @@
 package by.itAcademy.homeworks.loops;
 
-import java.text.DecimalFormat;
-import java.text.DecimalFormatSymbols;
-import java.text.NumberFormat;
-import java.util.Locale;
-import java.util.Scanner;
 
 /**
  * Задать произвольное число, вывести его в бухгалтерском формате.
@@ -12,17 +7,12 @@ import java.util.Scanner;
 
 public class Task17 {
     public static void main(String[] args) {
-//        DecimalFormat formater = (DecimalFormat) NumberFormat.getInstance();
-//        DecimalFormatSymbols symbols = formater.getDecimalFormatSymbols();
-//        symbols.setGroupingSeparator(' ');
-//        formater.setDecimalFormatSymbols(symbols);
-//        System.out.println(formater.format(value));
-        long l = 589798749;
+        long value = 544883949;
         String result = "";
         do {
-            result = l % 1000 + " " + result;
-            l = l / 1000;
-
-        } while (l > 0);
+            result = value % 1000 + " " + result;
+            value = value / 1000;
+        } while (value > 0);
+        System.out.println(result);
     }
 }
