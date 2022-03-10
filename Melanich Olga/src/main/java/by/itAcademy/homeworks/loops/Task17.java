25db60bc5a44f00d5ef99b0c17734afd1f934e9a
Olga Melanich <beekwin@gmail.com>
2022-03-10 16:54:39 +0300

    Task 17 есть вопросы, Task 18

--- Melanich Olga/src/main/java/by/itAcademy/homeworks/loops/Task17.java
+++ Melanich Olga/src/main/java/by/itAcademy/homeworks/loops/Task17.java
@@ -1,20 +1,76 @@
 package by.itAcademy.homeworks.loops;
 
-import by.itAcademy.homeworks.utils.Printer;
+import java.util.Arrays;
 
-import java.text.NumberFormat;
-import java.util.Scanner;
-
-/** Задание 17
+/**
+ * Задание 17
  * Задать произвольное целое число и вывести его в бухгалтерском формате.
  */
 
 public class Task17 {
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        Printer.print("Enter the number: ");
-        int number = in.nextInt();
-        NumberFormat format = NumberFormat.getInstance();
-        Printer.print(format.format(number));
+//        Scanner in = new Scanner(System.in);
+//        Printer.print("Enter the number: ");
+//        int number = in.nextInt();
+        int number = 422_675_768;
+
+        int length = getArrayLength(number);
+        int[] array = new int[length];
+        for (int i = array.length - 1; i >= 0; i--) {
+            array[i] = number % 1000;
+            number = number / 1000;
+            System.out.print(array[i] + " ");
+            // Как вывести нужный результат???
+        }
+        System.out.println(Arrays.toString(array));
+    }
+
+    public static int getArrayLength(int number) {
+        int divider = 1000;
+        int loopCounter = 1;
+        int counter = 0;
+        while (loopCounter > 0) {
+            loopCounter = number / divider;
+            counter++;
+            divider *= 1000;
+        }
+        return counter;
     }
 }
+
+
+//    public static String accountingFormat(int number, int counter) {
+//        String str = "";
+//        int a = 0;
+//        int b = 0;
+//        while (b >= counter) {
+//            a = number % 1000;
+//            number = number / 1000;
+//            str = " " + a;
+//            b++;
+//        }
+//        return str;
+//    }
+
+
+
+
+//        String s = " ";
+//        for (int i = number; i > 999; number  /= 1000) {
+//            int a = number % 1000;
+//
+//
+//        }
+//        return;
+//
+
+
+//        int lastThreeNumber = 0;
+//        while (number > 999) {
+//            lastThreeNumber = number % 1000;
+//
+//        }
+//        return lastThreeNumber;
+
+
+
