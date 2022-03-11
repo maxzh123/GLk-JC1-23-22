2ca193fe15afc809c0d1d38982e9ceca58a8da9d
Maksim S Zhalezny <M.Geleznu@novacom.by>
2022-03-11 16:51:23 +0300

    статистика

--- Examples/src/main/java/by/itAcademy/homeworks/basics/Task3.java
+++ Examples/src/main/java/by/itAcademy/homeworks/basics/Task3.java
@@ -1,4 +1,10 @@
 package by.itAcademy.homeworks.basics;
 
+import java.util.stream.Stream;
+
 public class Task3 {
+    public static void main(String[] args) {
+        String s="acca";
+        System.out.println(!Stream.iterate(0,(i)->++i).limit(s.length()/2).parallel().map((i)->s.charAt(i)!=s.charAt(s.length()-1-i)).anyMatch((i)->i==true));
+    }
 }
