f12ccd4fed47e7d0c0c01d755724e17b0b1e5ff4
Vlad <vladryabkov1998@gmail.com>
2022-03-11 20:22:03 +0300

    Задания 18-19

--- Ryabkov Vlad/src/main/java/by/itAcademy/homeworks/arrays/Task19.java
+++ Ryabkov Vlad/src/main/java/by/itAcademy/homeworks/arrays/Task19.java
@@ -1,4 +1,31 @@
 package by.itAcademy.homeworks.arrays;
 
+import java.util.Arrays;
+
+/**
+ * Создать переменную для массива из 10 элементов (другим способом).
+ * Заполнить его произвольными значениями целочисленного типа и вывести на
+ * экран элементы, стоящие на четных позициях.
+ */
+
 public class Task19 {
+    public static void main(String[] args) {
+        int[] array = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+        int[] array1 = randomarray(array);
+        System.out.println(Arrays.toString(array1));
+        for (int i = 0; i < array1.length; i++) {
+            if (array1[i] % 2 == 0) {
+                System.out.println("Делится на 2 : " + array1[i]);
+            }
+        }
+    }
+
+
+
+    public static int[] randomarray(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            array[i] = ((int) (Math.random() * 100) + 1);
+        }
+        return array;
+    }
 }
