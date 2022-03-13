176042bacd0eb44e5d9be4488a1eae7a638a8c9a
ArtyrBr <brilia08@example.com>
2022-03-13 14:11:37 +0300

    Задание 10

--- Artur Brylkou/src/main/java/by/itAcademy/homeworks/operators/Task10.java
+++ Artur Brylkou/src/main/java/by/itAcademy/homeworks/operators/Task10.java
@@ -1,4 +1,32 @@
 package by.itAcademy.homeworks.operators;
 
+
+import java.util.Scanner;
+
 public class Task10 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("SideA");
+        int sideA = sc.nextInt();
+        System.out.println("SideB");
+        int sideB = sc.nextInt();
+        System.out.println("SideC");
+        int sideC = sc.nextInt();
+        System.out.println("SideD");
+        int sideD = sc.nextInt();
+        System.out.println("SideE");
+        int sideE = sc.nextInt();
+        System.out.println("SideF");
+        int sideF = sc.nextInt();
+        System.out.println(chekIn(sideA,sideB,sideC,sideD,sideE,sideF));
+    }
+    public static String chekIn(int a, int b, int c, int d, int e, int f) {
+        if ((a + c) <= e && b <= f && d <= f) {
+            if ((a+b) <= f && b <= e && d <= e)
+        return ("Дома помещаются");
+                } else return "Дома не помещаются";
+
+
+        return "Дома помещаются";
+    }
 }
