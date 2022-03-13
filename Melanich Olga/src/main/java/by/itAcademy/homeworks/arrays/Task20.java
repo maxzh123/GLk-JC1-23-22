b9cd0ce24aac92286ada7656a21381766e36252f
Olga Melanich <beekwin@gmail.com>
2022-03-13 20:32:38 +0300

    Update Task 20 and Test 20

--- Melanich Olga/src/main/java/by/itAcademy/homeworks/arrays/Task20.java
+++ Melanich Olga/src/main/java/by/itAcademy/homeworks/arrays/Task20.java
@@ -14,16 +14,28 @@ public class Task20 {
     public static void main(String[] args) {
         int[] array = new int[10];
         Printer.print(Arrays.toString(Task18.fillArray(array)));
-        Printer.print(maxArrayElement(array));
+        Printer.print("Максимальный элемнт массива: " + getMaxArrayElement(array));
+        Printer.print("Индекс максимального элемента массива: " + maxIndex(array));
     }
-
-    public static int maxArrayElement(int[] arr) {
-        int maxElement = 0;
+    // находим макс элемент массива
+    public static int getMaxArrayElement(int[] arr) {
+        int maxElement = arr[0];
         for (int i = 0; i < arr.length; i++) {
             if (arr[i] > maxElement) {
                 maxElement = arr[i];
             }
         }
         return maxElement;
+    }
+    //находим индекс макс элемента массива
+    public static int maxIndex(int[] arr) {
+        int maxIndex = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[0] < arr[i]) {
+                arr[0] = arr[i];
+                maxIndex = i;
+            }
+        }
+        return maxIndex;
     }
 }
