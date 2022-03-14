3529c470a03c42a018f4878429ff9b6455ad7f47
Ury123 <bykovskijura792@gmail.com>
2022-03-14 17:49:45 +0300

    задание 6

--- Bykovski Ura/src/main/java/by/itAcademy/homeworks/operators/Task7.java
+++ Bykovski Ura/src/main/java/by/itAcademy/homeworks/operators/Task7.java
@@ -1,4 +1,16 @@
 package by.itAcademy.homeworks.operators;
 
+import java.util.Scanner;
+
 public class Task7 {
+    public static void main(String[] args){
+        Scanner sc  = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int r = sc.nextInt();
+        if (r*r>=a*a+b*b)
+            System.out.println("Yes");
+        else
+            System.out.println("No");
+    }
 }
