2f39be2ad56e5ac4faf21c479c97058127342650
Roman Kuznetsov <mmamano@inbox.ru>
2022-03-15 13:24:41 +0300

    IO minor fixes and completed additional task

---
+++ Kuznetsov Roman/src/main/java/by/itAcademy/homeworks/oop/AdditionalProblem/Interfaces/ICard.java
@@ -0,0 +1,8 @@
+package by.itAcademy.homeworks.oop.AdditionalProblem.Interfaces;
+
+import java.math.BigDecimal;
+
+public interface ICard {
+    public boolean addMoney(BigDecimal money);
+    public boolean withdrawMoney(BigDecimal money);
+}
