f3f8101ab331ce89b672a763fd3f9c458da01389
ArtyrBr <brilia08@example.com>
2022-03-15 12:59:06 +0300

    Тест 7

--- Artur Brylkou/src/test/java/operators/test7.java
+++ Artur Brylkou/src/test/java/operators/test7.java
@@ -3,13 +3,11 @@ package operators;
 import by.itAcademy.homeworks.operators.Task7;
 import org.junit.jupiter.api.Test;
 
-public class test7 {
+public class Test7 {
     @Test
     public void test1() {
-        Task7.closeCircle(1,2,3);
+        Task7.closeCircle(1, 2, 3);
     }
     @Test
-    public void test2() {
-        Task7.closeCircle(6,5,2);
-    }
+    public void test2() {Task7.closeCircle(5,4,1);}
 }
