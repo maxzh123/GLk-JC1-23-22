ed9ac0c15640d6afd49deb888dd9c0e720e581ba
ArtyrBr <brilia08@example.com>
2022-03-15 14:06:06 +0300

    Задание 10 переделал

--- Artur Brylkou/src/main/java/by/itAcademy/homeworks/loops/Task15.java
+++ Artur Brylkou/src/main/java/by/itAcademy/homeworks/loops/Task15.java
@@ -1,4 +1,25 @@
 package by.itAcademy.homeworks.loops;
 
 public class Task15 {
+    public static void main(String[] args) {
+       int simple = 0;
+       int number = 0;
+       for(int i =50; i<=70; i++){
+           if (correct(i)){
+               number++;
+           } if(number == 2){
+               simple = i;
+               break;
+           }
+       }
+        System.out.println("Second simple: " + simple);
+    }
+    public static boolean correct(int b){
+        for(int i = 2; i<b; i++){
+            if(b%i == 0){
+                return false;
+            }
+        }return true;
+
+    }
 }
