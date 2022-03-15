d88a63416a1ccfadd64be5663b1813dc5c62f238
ArtyrBr <brilia08@example.com>
2022-03-15 12:42:39 +0300

    Задание 10 переделал

---
+++ Artur Brylkou/src/test/java/operators/test7.java
@@ -0,0 +1,15 @@
+package operators;
+
+import by.itAcademy.homeworks.operators.Task7;
+import org.junit.jupiter.api.Test;
+
+public class test7 {
+    @Test
+    public void test1() {
+        Task7.closeCircle(1,2,3);
+    }
+    @Test
+    public void test2() {
+        Task7.closeCircle(6,5,2);
+    }
+}
