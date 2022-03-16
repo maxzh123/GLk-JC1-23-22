f9bf9ce6c6a87f3b49747e6d39af9c32bd9ec9ec
Ury123 <bykovskijura792@gmail.com>
2022-03-16 13:25:54 +0300

    задание 8(исправленное)

--- Bykovski Ura/src/main/java/by/itAcademy/homeworks/operators/Task8.java
+++ Bykovski Ura/src/main/java/by/itAcademy/homeworks/operators/Task8.java
@@ -7,14 +7,17 @@ public class Task8 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
-        if (a%10 == 0) {
+        if (a>=10 & a<=20){
             System.out.println(a + " рублей");
-        } else if (a%10 == 1) {
-            System.out.println(a + " рубль");
-        } else if (a%10>=2 & a%10<=4) {
-            System.out.println(a + " рубля");
-        } else if (a%10>=5 & a%10<=9) {
-            System.out.println(a + " рублей");
+        }
+        else if (a%10 == 0) {
+                 System.out.println(a + " рублей");
+             } else if (a%10 == 1) {
+                 System.out.println(a + " рубль");
+             } else if (a%10>=2 & a%10<=4) {
+                 System.out.println(a + " рубля");
+             } else if (a%10>=5 & a%10<=9) {
+                 System.out.println(a + " рублей");
         }
     }
 }
