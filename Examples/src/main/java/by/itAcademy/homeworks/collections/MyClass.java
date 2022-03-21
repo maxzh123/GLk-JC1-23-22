47cde8137c4775ac9418985c569123f9a032e472
IT-Academy-Gomel <IT-Academy-Gomel>
2022-03-21 21:36:21 +0300

    примеры

---
+++ Examples/src/main/java/by/itAcademy/homeworks/collections/MyClass.java
@@ -0,0 +1,13 @@
+package by.itAcademy.homeworks.collections;
+
+public class MyClass<TYPE> {
+    public TYPE getValue() {
+        return value;
+    }
+
+    public void setValue(TYPE value) {
+        this.value = value;
+    }
+
+    private TYPE value;
+}
