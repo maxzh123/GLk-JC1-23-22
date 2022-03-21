dacbe81a3b6e90e2ef691df90f5f33b44d4db12b
Olga Melanich <beekwin@gmail.com>
2022-03-21 14:46:03 +0300

    Task 24

---
+++ Melanich Olga/src/main/java/by/itAcademy/homeworks/objects/TimeInterval.java
@@ -0,0 +1,47 @@
+package by.itAcademy.homeworks.objects;
+
+/**
+ * Задание 24
+ * Создать класс, описывающий промежуток времени. Промежуток в классе должен задаваться тремя свойствами: секундами, минутами, часами.
+ * Сделать методы для получения полного количества секунд в объекте, сравнения двух объектов (аналогично compareTo в строках).
+ * Создать два конструктора: получающий общее количество секунд и получающий часы, минуты и секунды по отдельности.
+ * Сделать метод для вывода данных.
+ */
+
+public class TimeInterval implements Comparable<TimeInterval> {
+    private final int seconds;
+    private final int minutes;
+    private final int hours;
+
+    public TimeInterval(int seconds, int minutes, int hours) {
+        this.seconds = seconds;
+        this.minutes = minutes;
+        this.hours = hours;
+    }
+
+    public TimeInterval(int totalNumberOfSeconds) {
+        this.seconds = totalNumberOfSeconds % 60;
+        this.minutes = (totalNumberOfSeconds % 3600) / 60;
+        this.hours = totalNumberOfSeconds / 3600;
+    }
+
+    public int getTotalNumberOfSeconds() {
+        return this.seconds + (this.minutes * 60) + (this.hours * 3600);
+    }
+
+    @Override
+    public int compareTo(TimeInterval o) {
+        int first = getTotalNumberOfSeconds();
+        int second = o.getTotalNumberOfSeconds();
+        return first - second;
+    }
+
+    @Override
+    public String toString() {
+        return "TimeInterval{" +
+                "seconds=" + seconds +
+                ", minutes=" + minutes +
+                ", hours=" + hours +
+                '}';
+    }
+}
