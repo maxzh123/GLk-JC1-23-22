96db3f57599a759048f5c95a23c0d0ce3071e45a
Леванчуков Сергей <leva@example.com>
2022-03-23 21:27:19 +0300

    25 my attempt approaching infinity :(

---
+++ Levanchukov Sergey/src/main/java/by/itAcademy/homeworks/objects/Amount.java
@@ -0,0 +1,12 @@
+package by.itAcademy.homeworks.objects;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class Amount {
+    Collection collection=new ArrayList<>();
+
+    public void setCollection(Collection collection) {
+        this.collection = collection;
+    }
+}
