1fca3b6d8d25bdf0fc26045fd9f767864cdb95ea
anton <anton@example.com>
2022-03-24 10:28:09 +0300

    Task 16 is ок

--- Sinelnik Anton/src/main/java/by/itAcademy/homeworks/loops/Task16.java
+++ Sinelnik Anton/src/main/java/by/itAcademy/homeworks/loops/Task16.java
@@ -1,4 +1,18 @@
 package by.itAcademy.homeworks.loops;
 
+/**
+ * Для целых чисел, которые делятся на 7 в диапазоне от 1 до 100, вывести на экран строку “Hope!”.
+ */
+
 public class Task16 {
+    public static void main(String[] args) {
+       hope(1,100);
+    }
+    public static void hope (int start, int end){
+        for (int i = start; i <= end ; i++) {
+            if (i % 7 == 0) {
+                System.out.println("int " + i + " Hope!");
+            }
+        }
+    }
 }
