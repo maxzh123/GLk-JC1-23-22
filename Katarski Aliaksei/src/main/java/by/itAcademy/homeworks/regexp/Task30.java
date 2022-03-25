84469ea2ef4301f9726cda588613c405fae1712a
Katarski Aliaksei <Katarski Aliaksei>
2022-03-25 20:04:51 +0300

    task 28,29,30 done

--- Katarski Aliaksei/src/main/java/by/itAcademy/homeworks/regexp/Task30.java
+++ Katarski Aliaksei/src/main/java/by/itAcademy/homeworks/regexp/Task30.java
@@ -1,4 +1,23 @@
 package by.itAcademy.homeworks.regexp;
 
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * Введите с клавиатуры строку. Напишите метод, выполняющий поиск в строке
+ * > всех тегов абзацев, в том числе тех, у которых есть параметры, например, `<p
+ * > id=”p1”>`, и замену их на простые теги абзацев `<p>`. Результат работы метода
+ * > выведите на экран.
+ */
+
 public class Task30 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        String parReplace=sc.nextLine();
+        paragraphReplace(parReplace);
+    }
+    public static void paragraphReplace(String string){
+        System.out.println(string.replaceAll("<[а-яА-Яa-zA-ZёЁ]\\s?[^>]+>","<p>"));
+    }
 }
