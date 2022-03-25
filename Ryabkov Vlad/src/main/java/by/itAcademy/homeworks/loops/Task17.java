e5cf6503a6ff01cc1abe805dec0dca64c02f3e6b
Vlad <vladryabkov1998@gmail.com>
2022-03-25 13:00:31 +0300

    Task 17

--- Ryabkov Vlad/src/main/java/by/itAcademy/homeworks/loops/Task17.java
+++ Ryabkov Vlad/src/main/java/by/itAcademy/homeworks/loops/Task17.java
@@ -6,13 +6,21 @@ package by.itAcademy.homeworks.loops;
  */
 
 public class Task17 {
-   public static void main(String[] args) {
-        long value = 11000;
-        String result = "";
-        do {
-            result = value % 1000 + " " + result;
-            value = value / 1000;
-        } while (value > 0);
-        System.out.println(result);
+    public static void main(String[] args) {
+        long value = 75652;
+        addSpace(value);
+
+    }
+
+    static void addSpace(long value) {
+        StringBuffer str = new StringBuffer(Long.toString(value));
+        int i = str.length();
+        for (int j = i / 3 +1; j > 0; j--) {
+            str.insert(i, " ");
+            i -= 3;
+        }
+        System.out.println(str);
     }
 }
+
+
