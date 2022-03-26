67d582b3c4d8a49b40efcaa0de0e45ffacc9007e
leha <torlesh18112003@gmail.com>
2022-03-26 21:17:32 +0300

    completed task 40

--- Korotkevich Lyesha/src/main/java/by/itAcademy/homeworks/collections/Task40.java
+++ Korotkevich Lyesha/src/main/java/by/itAcademy/homeworks/collections/Task40.java
@@ -1,4 +1,27 @@
 package by.itAcademy.homeworks.collections;
 
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+/**Создать коллекцию, заполнить ее случайными целыми числами. Удалить повторяющиеся числа.*/
+
 public class Task40 {
+    public static void main(String[] args) {
+        List<Integer> collection = new ArrayList<>();
+        for (int i = 0; i < 100; i++) {
+            collection.add((int)(Math.random()*100)+1);
+        }
+        System.out.println(collection);
+        Set<Integer> collection2 = new HashSet<>();
+        collection2.addAll(collection);
+        collection.clear();
+        for(Integer i: collection2){
+            collection.add(i);
+        }
+        collection2.clear();
+        System.out.println(collection);
+    }
 }
