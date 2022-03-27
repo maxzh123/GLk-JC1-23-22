b7abf9780227f4264d4109fa1b2fbb6bf6d2a9d1
Леванчуков Сергей <leva@example.com>
2022-03-27 20:57:14 +0300

    39 done 40 not completely

--- Levanchukov Sergey/src/main/java/by/itAcademy/homeworks/collections/Task39.java
+++ Levanchukov Sergey/src/main/java/by/itAcademy/homeworks/collections/Task39.java
@@ -1,4 +1,23 @@
 package by.itAcademy.homeworks.collections;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+/**
+ * > Создать список оценок учеников с помощью ArrayList, заполнить случайными
+ * > оценками. Удалить неудовлетворительные оценки из списка.
+ * */
+
 public class Task39 {
+    public static void main(String[] args) {
+        ArrayInitialization listOfRatings=new ArrayInitialization();
+        Scanner s= new Scanner(System.in);
+
+        listOfRatings.setCount(s.nextInt());
+
+        listOfRatings.printArrayList(listOfRatings.deleteUnsatisfiedRatings(listOfRatings.arrayInitialization()));
+
+    }
+
 }
---
+++ Levanchukov Sergey/src/test/java/by/itAcademy/homeworks/collections/ArrayInitializationTest.java
@@ -0,0 +1,25 @@
+package by.itAcademy.homeworks.collections;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ArrayInitializationTest {
+
+    @Test
+    void printArrayList() {
+    }
+
+    @Test
+    void deleteUnsatisfiedRatings() {
+        List<Integer> listTest=new ArrayList<>();
+        listTest.add(5);
+        listTest.add(6);
+        listTest.add(2);
+        listTest.add(2);
+        listTest.add(6);
+    }
+}
