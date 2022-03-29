802d91615d31f41521e532adae4451763327f27d
Olga Melanich <beekwin@gmail.com>
2022-03-29 16:33:39 +0300

    regexp done

--- Melanich Olga/src/main/java/by/itAcademy/homeworks/regexp/Task29.java
+++ Melanich Olga/src/main/java/by/itAcademy/homeworks/regexp/Task29.java
@@ -1,4 +1,26 @@
 package by.itAcademy.homeworks.regexp;
 
+import by.itAcademy.homeworks.utils.Printer;
+
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * Задание 29
+ * Введите с клавиатуры строку. Напишите метод, выполняющий поиск в строке шестнадцатеричных чисел,
+ * записанных по правилам Java, с помощью регулярных выражений. Результат на экран.
+ */
+
 public class Task29 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String text = scanner.nextLine();
+        Pattern pattern = Pattern.compile("0X[0-9A-F]+|0x[0-9a-f]+");
+        Matcher matcher = pattern.matcher(text);
+        while (matcher.find()) {
+           Printer.print(matcher.group());
+        }
+    }
 }
+
