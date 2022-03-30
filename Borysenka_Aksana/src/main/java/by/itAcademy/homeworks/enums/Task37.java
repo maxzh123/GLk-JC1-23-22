40c72523c6267fdc25ce75f9627d0644fa0eb0a6
Aksana Barysenka <oksana_borisenko.95@example.com>
2022-03-31 01:30:48 +0300

    Enums is done

--- Borysenka_Aksana/src/main/java/by/itAcademy/homeworks/enums/Task37.java
+++ Borysenka_Aksana/src/main/java/by/itAcademy/homeworks/enums/Task37.java
@@ -1,4 +1,18 @@
 package by.itAcademy.homeworks.enums;
 
+import java.util.Scanner;
+
 public class Task37 {
+
+    /**Task37
+     * Написать метод, который в зависимости от сезона, выводит на экран сумму дней в этом сезоне.
+     * Входной параметр ввести с клавиатуры.
+     */
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Введи месяц");
+        Seasons seasons = Seasons.valueOf(sc.nextLine().toUpperCase());
+        System.out.println(seasons.getCountOfDays());
+    }
 }
