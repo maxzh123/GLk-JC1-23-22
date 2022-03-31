b58be5bd0c4a5e1d60f08dd74b24ff4d181c3334
ArtyrBr <brilia08@example.com>
2022-03-31 14:32:35 +0300

    Задание 27

--- Artur Brylkou/src/main/java/by/itAcademy/homeworks/regexp/Task27.java
+++ Artur Brylkou/src/main/java/by/itAcademy/homeworks/regexp/Task27.java
@@ -1,4 +1,20 @@
 package by.itAcademy.homeworks.regexp;
 
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Task27 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Введите текст");
+        String text = sc.nextLine();
+        Pattern p = Pattern.compile("[А-Яа-яЁёA-Za-z]+-?[А-Яа-яЁёA-Za-z]+");
+        Matcher m = p.matcher(text);
+        int count =0;
+        while (m.find()){
+            count++;
+        }
+        System.out.println("Количество слов "+count);
+    }
 }
