f499f01e3dd590757d99298c1ed3b760b504f36e
IT-Academy-Gomel <IT-Academy-Gomel>
2022-03-31 20:33:34 +0300

    пример

--- Examples/src/main/java/by/itAcademy/homeworks/datetime/Task64.java
+++ Examples/src/main/java/by/itAcademy/homeworks/datetime/Task64.java
@@ -1,8 +1,24 @@
 package by.itAcademy.homeworks.datetime;
 
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
+
+import java.time.*;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.TemporalAdjuster;
+import java.time.temporal.TemporalAdjusters;
+import java.util.Locale;
 
 public class Task64 {
+    public static void main(String[] args) {
+       DateTimeFormatter df=DateTimeFormatter.ofPattern("dd-MMMM-yyyy", Locale.CANADA_FRENCH);
+       LocalDate date =LocalDate.now();
+       System.out.println(date.format(df));
+       ZonedDateTime dt=ZonedDateTime .now();
+       System.out.println(dt);
+       Duration d=Duration.ofDays(5);
+
+       LocalDate nextSunday = date.with(TemporalAdjusters.next(DayOfWeek.SUNDAY));
+       System.out.println(nextSunday);
+
+
+    }
 }
