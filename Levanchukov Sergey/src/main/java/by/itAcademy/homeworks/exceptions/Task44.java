f6263c0422fbbf51f1b689f5ce61f36ece36dbe6
IT-Academy-Gomel <IT-Academy-Gomel>
2022-03-31 20:39:17 +0300

    Exceptions done

--- Levanchukov Sergey/src/main/java/by/itAcademy/homeworks/exceptions/Task44.java
+++ Levanchukov Sergey/src/main/java/by/itAcademy/homeworks/exceptions/Task44.java
@@ -1,4 +1,24 @@
 package by.itAcademy.homeworks.exceptions;
 
+/**
+ * > Написать собственное исключение от Exception. Сгенерировать код, который
+ * > будет выбрасывать его и обрабатывать. Результат работы программы вывести на
+ * > экран.
+ **/
+
 public class Task44 {
+    public static void main(String[] args) throws Exception {
+        firstTry();
+        System.out.println("Hello");
+    }
+
+    public static void firstTry() throws Exception{
+        try {
+            throw new ArrayIndexOutOfBoundsException("Выход за размер массива");
+        }
+        catch (ArrayIndexOutOfBoundsException e){
+            e.printStackTrace();
+        }
+    }
+
 }
--- Levanchukov Sergey/src/main/java/by/itAcademy/homeworks/exceptions/Task45.java
+++ Levanchukov Sergey/src/main/java/by/itAcademy/homeworks/exceptions/Task45.java
@@ -1,4 +1,26 @@
 package by.itAcademy.homeworks.exceptions;
 
+/**
+ * > Написать метод, который будет возбуждать исключение и обработать это
+ * > исключение на уровне выше. Результат работы программы вывести на экран.
+ **/
+
 public class Task45 {
+    public static void main(String[] args) throws Exception{
+       sumArray();
+    }
+
+    public static void sumArray (){
+        int [] array= new int [] {1,2,3,4,5};
+        try {
+            for (int i = 0; i < array.length + 1; i++) {
+                System.out.println(array[i]);
+            }
+
+        }
+        catch (ArrayIndexOutOfBoundsException e){
+            e.printStackTrace();
+        }
+    }
+
 }
