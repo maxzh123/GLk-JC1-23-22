4ba726d71d370867c1f64c7afe3d899b1134a188
Vlad <vladryabkov1998@gmail.com>
2022-04-04 12:53:23 +0300

    Tasks enums

--- Ryabkov Vlad/src/main/java/by/itAcademy/homeworks/enums/Task37.java
+++ Ryabkov Vlad/src/main/java/by/itAcademy/homeworks/enums/Task37.java
@@ -1,4 +1,17 @@
 package by.itAcademy.homeworks.enums;
 
+import java.util.Locale;
+import java.util.Scanner;
+
+/**
+ * Написать метод, который в зависимости от сезона, выводит на экран сумму
+ *  дней в этом сезоне. Входной параметр ввести с клавиатуры.
+ */
 public class Task37 {
+    public static void main(String[] args) {
+        System.out.println("Введите сезон года : ");
+        Scanner scanner = new Scanner(System.in);
+        Seasons seasons = Seasons.valueOf(scanner.nextLine().toUpperCase(Locale.ROOT));
+        System.out.println(seasons.getDaysCount());
+    }
 }
