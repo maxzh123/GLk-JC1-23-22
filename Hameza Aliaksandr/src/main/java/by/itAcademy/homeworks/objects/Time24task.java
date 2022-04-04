d1a6fa6e145c1a12273181391259eb609509d9fc
Aliaksandr Hameza <otulan@example.com>
2022-04-04 17:31:41 +0300

    Задание 24 в процессе

---
+++ Hameza Aliaksandr/src/main/java/by/itAcademy/homeworks/objects/Time24task.java
@@ -0,0 +1,13 @@
+package by.itAcademy.homeworks.objects;
+
+public class Time24task {
+    public int seconds;
+    public int minutes;
+    public int hours;
+    public Time (int second, int minutes, int hours) {
+        this.seconds=second;
+        this.minutes=minutes;
+        this.hours=hours;
+    }
+    public int TimeGetSeconds ()
+}
