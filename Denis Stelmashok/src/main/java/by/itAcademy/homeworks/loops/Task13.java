913d079314af7ca01f50e0c5b2e38e50bfb91dc8
DENCHIK <d.s.stelmashok@gmail.com>
2022-04-05 22:38:41 +0300

    задание 13

--- Denis Stelmashok/src/main/java/by/itAcademy/homeworks/loops/Task13.java
+++ Denis Stelmashok/src/main/java/by/itAcademy/homeworks/loops/Task13.java
@@ -1,4 +1,13 @@
 package by.itAcademy.homeworks.loops;
 
+/**Вычислить произведение чисел от 1 до 25 с помощью цикла do while.*/
 public class Task13 {
+    public static void main(String[] args) {
+        int x=25; int a=1;
+        do {
+        a*=x;
+        x--;
+        }while (x >= 1);
+        System.out.println("Произведение чисел " + a);
+    }
 }
