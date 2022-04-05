366371c3f83efd5d60843f28bd8a634292e1e321
DENCHIK <d.s.stelmashok@gmail.com>
2022-04-05 23:59:38 +0300

    задание 14

--- Denis Stelmashok/src/main/java/by/itAcademy/homeworks/loops/Task14.java
+++ Denis Stelmashok/src/main/java/by/itAcademy/homeworks/loops/Task14.java
@@ -1,4 +1,17 @@
 package by.itAcademy.homeworks.loops;
 
+import java.util.Scanner;
+
+/**Посчитать сумму цифр числа 7893823445 с помощью цикла do while.*/
 public class Task14 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        char num [] = scanner.nextLine().toCharArray();
+        int i=0; int resuit=0;
+        do {
+            resuit+= Integer.parseInt(String.valueOf(num[i]));
+            i++;
+        }while (i< num.length);
+        System.out.println("Summa numbers" +resuit);
+    }
 }
