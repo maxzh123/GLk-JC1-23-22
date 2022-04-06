3eb0115a277848c3a6a93b1b3fdac87eae6f7ddc
Aksana Barysenka <oksana_borisenko.95@example.com>
2022-04-06 23:09:21 +0300

    Task47 is done

--- Borysenka_Aksana/src/main/java/by/itAcademy/homeworks/io/Task47.java
+++ Borysenka_Aksana/src/main/java/by/itAcademy/homeworks/io/Task47.java
@@ -1,6 +1,8 @@
 package by.itAcademy.homeworks.io;
 
 import java.io.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Task47 {
 
@@ -10,10 +12,39 @@ public class Task47 {
      */
 
     public static void main(String[] args) throws IOException {
-        Reader r = new BufferedReader(new FileReader("Borysenka_Aksana\\src\\main\\resources\\ForTask47.txt.txt"));
-        do{
-            System.out.print((char)r.read());
-        }while (r.ready());
+        String str = "";
+        BufferedReader br = new BufferedReader(new FileReader("Borysenka_Aksana\\src\\main\\resources\\ForTask47.txt"));
+        if (br.equals(null)) {
+            System.out.println("Путь должен быть не null");
+        }else {
+            String strBr = br.readLine();
+            while (strBr != null) {
+                str += str + strBr + "\n";
+                strBr = br.readLine();
+            }
+            System.out.println("Amount of words: " + getCountWords(str));
+            System.out.println("Amount of punctuation marks: " + getPunctCount(str));
+        }
+    }
+
+    public static int getCountWords(String str) {
+        Pattern p = Pattern.compile("[А-Яа-яЁёA-Za-z]+(?:[-][А-Яа-яЁёA-Za-z]+)*", Pattern.MULTILINE);
+        Matcher m = p.matcher(str);
+        int count = 0;
+        while (m.find()){
+            count++;
+        }
+        return count;
+    }
+
+    public static int getPunctCount(String str){
+        int count = 0;
+        Pattern p = Pattern.compile("[\\.]{3}|[\\?|\\!][\\!\\?\\.]*|[\\.,!?:;\\-\"()]", Pattern.MULTILINE);
+        Matcher m = p.matcher(str);
+        while ((m.find())){
+            count++;
+        }
+        return count;
     }
 
 }
