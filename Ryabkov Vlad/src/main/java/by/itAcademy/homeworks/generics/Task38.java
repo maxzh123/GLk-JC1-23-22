a37f1cf30de0ff1ba077540eaca9194a2ebe5883
Vlad <vladryabkov1998@gmail.com>
2022-04-07 16:06:16 +0300

    Task 38

--- Ryabkov Vlad/src/main/java/by/itAcademy/homeworks/generics/Task38.java
+++ Ryabkov Vlad/src/main/java/by/itAcademy/homeworks/generics/Task38.java
@@ -1,4 +1,17 @@
 package by.itAcademy.homeworks.generics;
 
+import by.itAcademy.homeworks.generics.Transport.Car;
+import by.itAcademy.homeworks.generics.Transport.Garage;
+import by.itAcademy.homeworks.generics.Transport.Moto;
+import by.itAcademy.homeworks.generics.Transport.Vehicle;
+
 public class Task38 {
+    public static void main(String[] args) {
+        Garage<Vehicle> garage1 = new Garage<>();
+        Garage<Vehicle> garage2 = new Garage<>();
+        garage1.addVehicle(new Car("Renault","Megane"));
+        garage2.addVehicle(new Moto("Yamaha","R1"));
+        System.out.println("В первом гараже : " + garage1);
+        System.out.println("Во втором гараже : " + garage2);
+    }
 }
