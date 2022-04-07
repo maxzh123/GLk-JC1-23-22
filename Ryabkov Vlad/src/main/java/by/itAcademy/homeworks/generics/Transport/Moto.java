82e336f22381d4da9e14607b591cdc4df9ccc966
Vlad <vladryabkov1998@gmail.com>
2022-04-07 16:05:51 +0300

    Task 38

---
+++ Ryabkov Vlad/src/main/java/by/itAcademy/homeworks/generics/Transport/Moto.java
@@ -0,0 +1,10 @@
+package by.itAcademy.homeworks.generics.Transport;
+
+public class Moto extends Vehicle{
+    public Moto(String brand, String model){
+        super("Мотоцикл");
+        this.brand = brand;
+        this.model = model;
+    }
+
+}
