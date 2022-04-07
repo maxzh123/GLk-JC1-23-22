6fe0ab4531d1c86cb3ae9a7456804d0792fdbaf2
Olga Melanich <beekwin@gmail.com>
2022-04-07 15:30:18 +0300

    Task 44

--- Melanich Olga/src/main/java/by/itAcademy/homeworks/exceptions/Task44.java
+++ Melanich Olga/src/main/java/by/itAcademy/homeworks/exceptions/Task44.java
@@ -1,4 +1,24 @@
 package by.itAcademy.homeworks.exceptions;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Задание 44
+ * Написать собственное исключение от Exception. Сгенерировать код, который будет выбрасывать его и обрабатывать.
+ * Результат работы программы вывести на экран.
+ */
+
 public class Task44 {
+    public static void main(String[] args) {
+        try {
+            throw new MyException("MyException");
+        } catch (MyException e) {
+            System.err.println(e.getMessage());
+        }
+    }
 }
