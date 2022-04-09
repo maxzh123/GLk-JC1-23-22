7d9b6824606cc6b4a6187ccad65a5ab2812fb72c
leha <torlesh18112003@gmail.com>
2022-04-09 23:52:04 +0300

    completed dateTime

--- Korotkevich Lyesha/src/main/java/by/itAcademy/homeworks/datetime/Task64.java
+++ Korotkevich Lyesha/src/main/java/by/itAcademy/homeworks/datetime/Task64.java
@@ -1,4 +1,23 @@
 package by.itAcademy.homeworks.datetime;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.TemporalAdjusters;
+import java.util.Scanner;
+
+/**Ввести с клавиатуры номер месяца текущего года. Вывести на экран все его даты в формате d::MMM::uuuu.*/
+
 public class Task64 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        DateTimeFormatter df = DateTimeFormatter.ofPattern("d::MMM::uuuu");
+        int monthNumber = sc.nextInt();
+        LocalDate localDate = LocalDate.now();
+        LocalDate localDate1 = LocalDate.of(localDate.getYear(), monthNumber, 1);
+        while(localDate1.with(TemporalAdjusters.lastDayOfMonth()).getDayOfMonth() > (localDate1.getDayOfMonth())){
+            System.out.println(df.format(localDate1));
+            localDate1 = localDate1.plusDays(1);
+        }
+        System.out.println(df.format(localDate1));
+    }
 }
--- Korotkevich Lyesha/src/main/java/by/itAcademy/homeworks/datetime/Task66.java
+++ Korotkevich Lyesha/src/main/java/by/itAcademy/homeworks/datetime/Task66.java
@@ -1,4 +1,12 @@
 package by.itAcademy.homeworks.datetime;
 
+import java.time.LocalDate;
+
+/**От текущей даты вывести на экран дату, которая была 60 дней назад.*/
+
 public class Task66 {
+    public static void main(String[] args) {
+        LocalDate ld = LocalDate.now();
+        System.out.println(ld.minusDays(60));
+    }
 }
