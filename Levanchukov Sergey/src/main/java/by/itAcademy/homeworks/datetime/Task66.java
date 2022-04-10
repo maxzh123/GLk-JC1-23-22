aad4635ae3190304abd7d6475c11d33348016e74
Леванчуков Сергей <leva@example.com>
2022-04-10 17:50:04 +0300

    65, 66 done

--- Levanchukov Sergey/src/main/java/by/itAcademy/homeworks/datetime/Task66.java
+++ Levanchukov Sergey/src/main/java/by/itAcademy/homeworks/datetime/Task66.java
@@ -1,4 +1,19 @@
 package by.itAcademy.homeworks.datetime;
 
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
+/** Задание 66
+ * От текущей даты вывести на экран дату, которая была 60 дней назад.
+ */
+
 public class Task66 {
+    public static void main(String[] args) {
+        Calendar calendar=new GregorianCalendar();
+        System.out.println(calendar.getTime());
+        calendar.roll(Calendar.DAY_OF_YEAR,-60);
+        System.out.println(calendar.getTime());
+    }
 }
