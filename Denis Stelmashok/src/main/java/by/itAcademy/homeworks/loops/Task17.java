79b1cc53f66dc3ff645fd5bdf2b3d68112869c30
DENCHIK <d.s.stelmashok@gmail.com>
2022-04-10 20:27:36 +0300

    задание 17

--- Denis Stelmashok/src/main/java/by/itAcademy/homeworks/loops/Task17.java
+++ Denis Stelmashok/src/main/java/by/itAcademy/homeworks/loops/Task17.java
@@ -1,4 +1,16 @@
 package by.itAcademy.homeworks.loops;
 
+import java.text.NumberFormat;
+import java.util.Scanner;
+
+/**> Задать произвольное целое число и вывести его в бухгалтерском формате, то
+        > есть, начиная справа, каждые три позиции отделяются пробелом. Например, число
+        > 20023143 должно быть выведено как 20 023 143.*/
 public class Task17 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int num= sc.nextInt();
+        NumberFormat f=NumberFormat.getInstance();
+        System.out.println(f.format(num));
+    }
 }
