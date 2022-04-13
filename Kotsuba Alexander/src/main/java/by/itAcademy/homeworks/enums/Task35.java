0b077e08e03c273cd95c9bcc8f6c5e2e3c091fe0
Alexander8613-stack <kocuba-aleksandr@mail.ru>
2022-04-14 00:35:32 +0300

    Done

--- Kotsuba Alexander/src/main/java/by/itAcademy/homeworks/enums/Task35.java
+++ Kotsuba Alexander/src/main/java/by/itAcademy/homeworks/enums/Task35.java
@@ -1,4 +1,9 @@
 package by.itAcademy.homeworks.enums;
 
 public class Task35 {
+    public static void main(String[] args) {
+       for(Seasons seasons : Seasons.values()){
+           System.out.println(Seasons.getInfo(seasons));
+       }
+    }
 }
