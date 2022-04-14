8d2cdadc109f66f97aa82fdf0f44b0fed959c350
Alexander8613-stack <kocuba-aleksandr@mail.ru>
2022-04-15 00:59:11 +0300

    Done

--- Kotsuba Alexander/src/main/java/by/itAcademy/homeworks/enums/Task36.java
+++ Kotsuba Alexander/src/main/java/by/itAcademy/homeworks/enums/Task36.java
@@ -2,6 +2,7 @@
 Входной параметр ввести с клавиатуры.*/
 package by.itAcademy.homeworks.enums;
 
+import java.util.Locale;
 import java.util.Scanner;
 
 public class Task36 {
@@ -9,6 +10,11 @@ public class Task36 {
         System.out.println("Enter the name of the season: ");
         Scanner sc = new Scanner(System.in);
         String season = sc.nextLine();
-        SeasonsNumberTwo.getNextSeason(season);
+       // SeasonsNumberTwo.getNextSeason(season);
+    }
+
+    public static void nextM(String m){
+        SeasonsNumberTwo seasons = SeasonsNumberTwo.valueOf(m.toUpperCase(Locale.ROOT));
+        seasons.getNextSeason(seasons);
     }
 }
