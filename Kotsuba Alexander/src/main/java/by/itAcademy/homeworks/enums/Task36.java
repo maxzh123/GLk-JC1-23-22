11266506737d7f2ac507e05c171ed0351d29e492
Alexander8613-stack <kocuba-aleksandr@mail.ru>
2022-04-15 01:00:15 +0300

    Done

--- Kotsuba Alexander/src/main/java/by/itAcademy/homeworks/enums/Task36.java
+++ Kotsuba Alexander/src/main/java/by/itAcademy/homeworks/enums/Task36.java
@@ -9,7 +9,7 @@ public class Task36 {
     public static void main(String[] args) {
         System.out.println("Enter the name of the season: ");
         Scanner sc = new Scanner(System.in);
-        String season = sc.nextLine();
+        nextM(sc.nextLine());
        // SeasonsNumberTwo.getNextSeason(season);
     }
 
