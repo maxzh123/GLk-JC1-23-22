9dc099d8730ba19d2390fd444128ac5c2d54dd43
Aliaksandr Hameza <otulan@example.com>
2022-04-14 18:27:24 +0300

    Задание 26-29 выполнены.

--- Hameza Aliaksandr/src/main/java/by/itAcademy/homeworks/regexp/Task28.java
+++ Hameza Aliaksandr/src/main/java/by/itAcademy/homeworks/regexp/Task28.java
@@ -1,4 +1,25 @@
 package by.itAcademy.homeworks.regexp;
 
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * Задание 28
+ * Введите с клавиатуры текст. Выведите на экран текст, составленный из последних букв всех слов из исходного текста.
+ */
+
 public class Task28 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Введите текст: ");
+        String str = scan.nextLine();
+        String str1 = "";
+        Pattern p = Pattern.compile("[\\wА-Яа-яЁё]\\b");
+        Matcher m = p.matcher(str);
+        while (m.find()){
+            str1 += m.group();
+        }
+        System.out.println(str1);
+    }
 }
--- Hameza Aliaksandr/src/main/java/by/itAcademy/homeworks/regexp/Task29.java
+++ Hameza Aliaksandr/src/main/java/by/itAcademy/homeworks/regexp/Task29.java
@@ -1,4 +1,23 @@
 package by.itAcademy.homeworks.regexp;
 
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * Задание 29
+ * Введите с клавиатуры строку. Напишите метод, выполняющий поиск в строке шестнадцатеричных чисел,
+ * записанных по правилам Java, с помощью регулярных выражений. Результат работы метода выведите на экран.
+ */
+
 public class Task29 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String str = scan.nextLine();
+        Pattern p = Pattern.compile("0[xX][0-9A-Fa-f]{2}");
+        Matcher m = p.matcher(str);
+        while (m.find()) {
+            System.out.println(m.group());
+        }
+    }
 }
