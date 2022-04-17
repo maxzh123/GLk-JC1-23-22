c7e952b16604bb2dfb69088f331c2edff53de895
leha <torlesh18112003@gmail.com>
2022-04-17 23:42:18 +0300

    completed io

--- Korotkevich Lyesha/src/main/java/by/itAcademy/homeworks/io/Task51.java
+++ Korotkevich Lyesha/src/main/java/by/itAcademy/homeworks/io/Task51.java
@@ -1,4 +1,36 @@
 package by.itAcademy.homeworks.io;
 
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+
+/**Создать объект Person c полями name, surname, age. Сгенерировать 10 объектов класса Person со случайными полями соответствующего типа.
+С помощью Java создать файл, в который запишется информация о этих людях.*/
+
 public class Task51 {
+    public static void main(String[] args) {
+        Person[] people = new Person[10];
+        for (int i = 0; i < people.length; i++){
+            people[i] = new Person(generateString(), generateString(), generateInt(100));
+        }
+        try (ObjectOutputStream output = new ObjectOutputStream(new FileOutputStream("Korotkevich Lyesha/src/main/resources/Task51.bin"))) {
+            output.writeObject(people);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static int generateInt(int n){
+        return (int)(Math.random()*n+1);
+    }
+
+    public static String generateString(){
+        int n = generateInt(10);
+        char[] letters = "ABCDEFGHIKLMNOPQRSTVXYZ".toLowerCase().toCharArray();
+        char[] str = new char[n];
+        for (int i = 0; i < n; i++){
+            str[i] = letters[generateInt(23)-1];
+        }
+        return str.toString();
+    }
 }
