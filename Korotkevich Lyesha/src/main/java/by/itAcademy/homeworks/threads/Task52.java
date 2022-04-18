7fd83992ba7f9f9b2e4c6ec179f438631820da69
leha <torlesh18112003@gmail.com>
2022-04-18 23:26:18 +0300

    Не уверен

--- Korotkevich Lyesha/src/main/java/by/itAcademy/homeworks/threads/Task52.java
+++ Korotkevich Lyesha/src/main/java/by/itAcademy/homeworks/threads/Task52.java
@@ -1,4 +1,13 @@
 package by.itAcademy.homeworks.threads;
 
+/** Создать 10 потоков, каждый из которых вычисляет среднее арифметическое
+коллекции из 10 случайных целых чисел и выводит на экран.*/
+
 public class Task52 {
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++){
+            Average t = new Average();
+            t.run();//наверное не так ,потоки веде не параллельно считают
+        }
+    }
 }
