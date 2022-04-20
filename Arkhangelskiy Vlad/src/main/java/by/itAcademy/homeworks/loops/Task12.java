14ba14e887ecef1a2d0e45a463fffd201f7f70ec
Vlad Arkhangelskiy <vladarkhangelskiy>
2022-04-21 01:05:40 +0300

    7,8,9,10,12

--- Arkhangelskiy Vlad/src/main/java/by/itAcademy/homeworks/loops/Task12.java
+++ Arkhangelskiy Vlad/src/main/java/by/itAcademy/homeworks/loops/Task12.java
@@ -1,4 +1,22 @@
 package by.itAcademy.homeworks.loops;
 
+/**
+ * Задание 12
+ * Вычислить факториал целых чисел от 0 до 10 с помощью цикла while
+ */
+
 public class Task12 {
+    public static void main(String[] args) {
+       System.out.println(factorial(10));
+    }
+    public static  int factorial(int a){
+        int i = 1;
+        int result = 1;
+        while (i <= a){
+            result = result * i;
+            i++;
+        }
+       return result;
+
+    }
 }
