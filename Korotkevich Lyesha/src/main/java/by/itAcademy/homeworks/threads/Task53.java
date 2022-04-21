e13319603d804b3ab3eda8aedb4c95f82763c508
leha <torlesh18112003@gmail.com>
2022-04-21 19:09:13 +0300

    task53

--- Korotkevich Lyesha/src/main/java/by/itAcademy/homeworks/threads/Task53.java
+++ Korotkevich Lyesha/src/main/java/by/itAcademy/homeworks/threads/Task53.java
@@ -1,4 +1,15 @@
 package by.itAcademy.homeworks.threads;
 
+/**Создать класс поток, который генерирует массив случайных целых чисел из 10 элементов, затем находит максимальный элемент,
+ * в этом массиве, и выводит на экран в формате имя потока, максимальный элемент. Запустить 10 потоков.
+ */
+
+
 public class Task53 {
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            Thread t = new Thread(new Max());
+            t.start();
+        }
+    }
 }
