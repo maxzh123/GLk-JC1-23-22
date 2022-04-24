45aaac4b54ae70ce5b2d1ec2d3ed1c28cbb5da0e
Alexander8613-stack <kocuba-aleksandr@mail.ru>
2022-04-24 20:29:41 +0300

    Done

--- Kotsuba Alexander/src/main/java/by/itAcademy/homeworks/exceptions/Task45.java
+++ Kotsuba Alexander/src/main/java/by/itAcademy/homeworks/exceptions/Task45.java
@@ -1,4 +1,16 @@
+/*Написать метод, который будет возбуждать исключение и обработать это исключение на уровне выше.
+Результат работы программы вывести на экран.*/
+
 package by.itAcademy.homeworks.exceptions;
 
 public class Task45 {
+    public static void main(String[] args) {
+        int x = 20;
+        int z = 0;
+        try {
+            System.out.println(x/z);
+        }catch (Exception e){
+            System.err.println("деление на 0");
+        }
+    }
 }
