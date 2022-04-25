a3ee50e29200c234931b386312947633cf1cef35
IT-Academy-Gomel <IT-Academy-Gomel>
2022-04-25 19:10:25 +0300

    пример

--- Denis Stelmashok/src/main/java/by/itAcademy/homeworks/lamda/Task62.java
+++ Denis Stelmashok/src/main/java/by/itAcademy/homeworks/lamda/Task62.java
@@ -1,15 +1,5 @@
 package by.itAcademy.homeworks.lamda;
 
 public class Task62 {
-    public static void main(String[] args) {
-        Task61 t1=new Task61();
-        Task61 t2=new Task61();
-        NumProvider n1=t1.createMyClass();
-        NumProvider n2=t1.createMyClass();
-        NumProvider n3=t2.createMyClass();
-        System.out.println(n1.getNum());
-        System.out.println(n2.getNum());
-        System.out.println(n3.getNum());
-        System.out.println(n1==n2);
-    }
+
 }
--- Examples/src/main/java/by/itAcademy/homeworks/lamda/Task63.java
+++ Examples/src/main/java/by/itAcademy/homeworks/lamda/Task63.java
@@ -3,4 +3,7 @@ package by.itAcademy.homeworks.lamda;
 import java.util.stream.Stream;
 
 public class   Task63 {
+    public static void main(String[] args) {
+
+    }
 }
