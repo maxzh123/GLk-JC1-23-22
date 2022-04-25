5d093f76c8ea5ed71ba1e2256ad8feead59a6fb8
Ury123 <bykovskijura792@gmail.com>
2022-04-25 17:55:05 +0300

    Задание 29

--- Bykovski Ura/src/main/java/by/itAcademy/homeworks/regexp/Task29.java
+++ Bykovski Ura/src/main/java/by/itAcademy/homeworks/regexp/Task29.java
@@ -1,4 +1,25 @@
 package by.itAcademy.homeworks.regexp;
 
+//Введите с клавиатуры строку. Напишите метод, выполняющий поиск в строке шестнадцатеричных чисел, записанных по правилам Java, с помощью регулярных выражений.
+// Результат работы метода выведите на экран.
+
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Task29 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        String s=sc.nextLine();
+        System.out.println(get16(s));
+    }
+    public static String get16(String s){
+        String s1="";
+        Pattern p=Pattern.compile("0x([0-9A-F]{2})+");
+        Matcher m= p.matcher(s);
+        while (m.find()){
+            s1+=m.group()+" ";
+        }
+        return s1;
+    }
 }
