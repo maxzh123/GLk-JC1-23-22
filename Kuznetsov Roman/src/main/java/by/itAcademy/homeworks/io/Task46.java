5cd031eb5878eb7b1cd6a167ee7f6d09d0f9d2dc
Roman Kuznetsov <roman_kuzn@mail.ru>
2022-04-28 19:19:52 +0300

    Completed task 46.

--- Kuznetsov Roman/src/main/java/by/itAcademy/homeworks/io/Task46.java
+++ Kuznetsov Roman/src/main/java/by/itAcademy/homeworks/io/Task46.java
@@ -2,29 +2,37 @@ package by.itAcademy.homeworks.io;
 
 import java.io.File;
 import java.nio.file.Files;
+import java.util.Arrays;
 import java.util.Objects;
 
 public class Task46 {
     public static void main(String[] args) {
         File path = new File(".");
-        outputListOfDirectory(path);
-        outputListOfFiles(path);
+        outputListOfDirectory(path,0);
+        outputListOfFiles(path,0);
     }
 
-    public static void outputListOfDirectory(File directory) {
-        System.out.println("Каталоги:");
-        for (File file : Objects.requireNonNull(directory.listFiles())) {
-            if (file.isDirectory()) {
-                System.out.println(file.getName());
-            }
+    public static void outputListOfDirectory(File directory, int step) {
+        for (File file : directory.listFiles()) {
+                if(file.isDirectory()){
+                    System.out.println(file.getName()+":");
+                    outputListOfFiles(file, step);
+                }
+                else {
+                    System.out.print("-");
+                    System.out.println(file.getName());
+                }
         }
     }
 
-    public static void outputListOfFiles(File directory) {
-        System.out.println("Файлы:");
+    public static void outputListOfFiles(File directory, int step) {
         for (File file : Objects.requireNonNull(directory.listFiles())) {
+            System.out.print("-");
             if (file.isFile()) {
                 System.out.println(file.getName());
+            }
+            else {
+                outputListOfDirectory(file,step++);
             }
         }
     }
