7add767a6ca8dfdbd193f2ba06a9ad31b44d8507
Vlad Arkhangelskiy <vladarkhangelskiy>
2022-04-28 17:14:06 +0300

    26,27

--- Arkhangelskiy Vlad/src/main/java/by/itAcademy/homeworks/regexp/Task26.java
+++ Arkhangelskiy Vlad/src/main/java/by/itAcademy/homeworks/regexp/Task26.java
@@ -1,4 +1,37 @@
 package by.itAcademy.homeworks.regexp;
 
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * Задание 26
+ * Введите с клавиатуры строку. Найти в строке не только запятые, но и другие знаки препинания. Подсчитать общее их количество.
+ */
+
 public class Task26 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Введите текст");
+        String str = sc.nextLine();
+
+        puncts(str);
+
+        //String str = "sdfsd 123 dsfds, dssd? sdfds !  sdfsd... , , , , . ";
+    }
+    public static void puncts(String str) {
+        Pattern pattern = Pattern.compile("\\.{3}|[\\p{Punct}]");
+        Matcher matcher = pattern.matcher(str);
+        int count = 0;
+        while (matcher.find()) {
+            count++;
+        }
+        System.out.println("Колличество знаков припинания: " + count);
+    }
+
 }
+
+
+
+
--- Arkhangelskiy Vlad/src/main/java/by/itAcademy/homeworks/regexp/Task27.java
+++ Arkhangelskiy Vlad/src/main/java/by/itAcademy/homeworks/regexp/Task27.java
@@ -1,4 +1,56 @@
 package by.itAcademy.homeworks.regexp;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * Задание 26
+ * Введите с клавиатуры строку. Найти в строке не только запятые, но и другие знаки препинания. Подсчитать общее их количество.
+ *
+ * Задание 27 Введите с клавиатуры текст.
+ * Подсчитать количество слов в тексте.
+ * Учесть, что слова могут разделяться несколькими пробелами, в начале и конце текста также могут быть пробелы, но могут и отсутствовать.
+ */
+
 public class Task27 {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Введите текст");
+        String str = reader.readLine();
+
+        searcher(str);
+
+        //String str = "  ТРАТТА,  dsfsdfsfsd sdfsdff авпавпв... sdsdf  1232 ereer  аываппхавпхавпхаепвх " + "em... чТО КУДА !     ";
+    }
+
+    public static void searcher (String str) {
+        int words = 0;
+        int puncts = 0;
+
+        Pattern punct = Pattern.compile("\\.{3}|[\\p{Punct}]");
+        String[] arrayText = str.split("\\s");
+        for (
+                String s : arrayText) {
+            int count = 0;
+            Matcher m = punct.matcher(s);
+            while (m.find()) {
+                ++count;
+            }
+            puncts += count;
+            if (count != s.length()) {
+                ++words;
+            }
+        }
+        System.out.println("Количество слов: " + words);
+        System.out.println("Колличество знаков припинания: " + puncts);
+    }
 }
+/**
+ * Можно сломать если между словом и троеточием поставить пробел(посчитает ... и как слово и как знак припинания) ,
+ * на другие знаки препинания отрабатывает корректно.
+ */
