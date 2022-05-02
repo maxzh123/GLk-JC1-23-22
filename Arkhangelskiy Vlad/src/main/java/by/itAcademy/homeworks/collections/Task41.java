a7fb62ec6c9185631593a1105823692cb8802ed1
Vlad Arkhangelskiy <vladarkhangelskiy>
2022-05-03 02:19:48 +0300

    24, 39,40,41

--- Arkhangelskiy Vlad/src/main/java/by/itAcademy/homeworks/collections/Task41.java
+++ Arkhangelskiy Vlad/src/main/java/by/itAcademy/homeworks/collections/Task41.java
@@ -1,4 +1,39 @@
 package by.itAcademy.homeworks.collections;
 
+/**
+ * Task 41
+ * Создать список оценок учеников с помощью ArrayList, заполнить случайными оценками.
+ * Найти самую высокую оценку с использованием итератора.
+ */
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.Random;
+
 public class Task41 {
+
+    public static void main(String[] args) {
+
+        ArrayList<Integer> numbers = new ArrayList<>();
+        Random random = new Random();
+        for (int i = 0; i < 30; i++) {
+            numbers.add(random.nextInt(11));
+        }
+
+        System.out.println(getMax(numbers));
+
+    }
+
+    public static int getMax(ArrayList<Integer> numbers) {
+
+        Iterator<Integer> iterator = numbers.iterator();
+        int maxMark = iterator.next();
+        while (iterator.hasNext()) {
+            int element = iterator.next();
+            if (element > maxMark) {
+                maxMark = element;
+            }
+        }
+        return maxMark;
+    }
 }
