dd8d235c9ed77d3b5b70d910c7256339e44c3bf3
gamezo90 <gamezo90@mail.ru>
2022-05-05 15:49:39 +0300

    threads -> Task55 (not complete)

--- Gamezo Nikolai/src/main/java/by/itAcademy/homeworks/threads/Task55.java
+++ Gamezo Nikolai/src/main/java/by/itAcademy/homeworks/threads/Task55.java
@@ -1,4 +1,17 @@
 package by.itAcademy.homeworks.threads;
 
+/**
+ * Задание 55
+ * Создать метод, который печатает название потока и засыпает на 2 секунды.
+ * Запустить одновременно 10 потоков. Реализовать механизм синхронизации, чтобы все потоки выполнились последовательно.
+ */
+
 public class Task55 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) {
+            Thread t = new Thread (new Task54.CreateAndWrite());
+            t.setName("Thread " + i);
+            t.start();
+        }
+    }
 }
