35a17bb531749b8271b9c349f247ee961db84c72
Ury123 <bykovskijura792@gmail.com>
2022-05-09 19:46:55 +0300

    Задания 39

--- Bykovski Ura/src/main/java/by/itAcademy/homeworks/collections/Task39.java
+++ Bykovski Ura/src/main/java/by/itAcademy/homeworks/collections/Task39.java
@@ -1,4 +1,19 @@
 package by.itAcademy.homeworks.collections;
 
+//Создать список оценок учеников с помощью ArrayList, заполнить случайными оценками.
+//Удалить неудовлетворительные оценки из списка.
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Task39 {
+    public static void main(String[] args) {
+        List<Integer> arrayList=new ArrayList<>();
+        for (int i=0;i<15;i++){
+            arrayList.add((int)(Math.random()*10)+1);
+        }
+        System.out.println(arrayList);
+        arrayList.removeIf(i ->(i<5));
+        System.out.println(arrayList);
+    }
 }
