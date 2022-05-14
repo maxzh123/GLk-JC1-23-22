64c70495e90f100412b089ccdb974c69a468ee8c
Ury123 <bykovskijura792@gmail.com>
2022-05-14 21:47:45 +0300

    Задания 44

---
+++ Bykovski Ura/src/main/java/by/itAcademy/homeworks/exceptions/MyExeption.java
@@ -0,0 +1,7 @@
+package by.itAcademy.homeworks.exceptions;
+
+public class MyExeption extends Exception{
+    public MyExeption(String message) {
+        super(message);
+    }
+}
--- Bykovski Ura/src/main/java/by/itAcademy/homeworks/exceptions/Task44.java
+++ Bykovski Ura/src/main/java/by/itAcademy/homeworks/exceptions/Task44.java
@@ -1,4 +1,16 @@
 package by.itAcademy.homeworks.exceptions;
 
+//Написать собственное исключение от Exception.
+//Сгенерировать код, который будет выбрасывать его и обрабатывать.
+//Результат работы программы вывести на экран.
+
 public class Task44 {
+    public static void main(String[] args){
+        try {
+            throw new MyExeption("Ошибка");
+        } catch (MyExeption e) {
+            e.printStackTrace();
+        }
+        System.out.println("Оно работает");
+    }
 }
