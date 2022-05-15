1131fd7a95071729861c1d7bc9583baf6975822c
Ury123 <bykovskijura792@gmail.com>
2022-05-15 20:37:03 +0300

    Задания 45

--- Bykovski Ura/src/main/java/by/itAcademy/homeworks/exceptions/Task45.java
+++ Bykovski Ura/src/main/java/by/itAcademy/homeworks/exceptions/Task45.java
@@ -1,4 +1,16 @@
 package by.itAcademy.homeworks.exceptions;
 
+//Написать метод, который будет возбуждать исключение и обработать это исключение на уровне выше.
+//Результат работы программы вывести на экран.
+
 public class Task45 {
+    public static void main(String[] args) {
+        int[] array=new int[3];
+        try{
+            array[4]=5;
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+        System.out.println("работает");
+    }
 }
