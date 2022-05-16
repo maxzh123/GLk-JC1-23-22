8edbeddb2dae3649e94a4828fc3a637b4d76c245
Ury123 <bykovskijura792@gmail.com>
2022-05-16 20:03:28 +0300

    Задания 40 изменённое

--- Bykovski Ura/src/main/java/by/itAcademy/homeworks/collections/Task40.java
+++ Bykovski Ura/src/main/java/by/itAcademy/homeworks/collections/Task40.java
@@ -3,24 +3,25 @@ package by.itAcademy.homeworks.collections;
 //Создать коллекцию, заполнить ее случайными целыми числами. Удалить повторяющиеся числа.
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 public class Task40 {
     public static void main(String[] args) {
-        List<Integer> arrayList=new ArrayList<>();
-        for (int i=0;i<15;i++) {
+        List<Integer> arrayList = new ArrayList<>();
+        List<Integer> arrayList2 = new ArrayList<>();
+        int i;
+        for (i = 0; i < 10; i++) {
             arrayList.add((int) (Math.random() * 20) + 1);
         }
         System.out.println(arrayList);
         int a;
-        for (int i=0;i<arrayList.size();i++){
+        for (i=0;i<arrayList.size();i++){
             a=arrayList.get(i);
-            for (int j=i+1;j<arrayList.size();j++){
-                if (arrayList.get(j)==a){
-                    arrayList.remove(j);
-                }
+            if (arrayList2.indexOf(a)==-1){
+                arrayList2.add(a);
             }
         }
-        System.out.println(arrayList);
+        System.out.println(arrayList2);
     }
 }
