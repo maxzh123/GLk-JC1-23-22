c272f221c073ecb169da22efa222a1f6b3f93c5a
IT-Academy-Gomel <IT-Academy-Gomel>
2022-05-16 20:18:08 +0300

    пример

--- Borysenka_Aksana/src/main/java/by/itAcademy/homeworks/patterns/MySingleton.java
+++ Borysenka_Aksana/src/main/java/by/itAcademy/homeworks/patterns/MySingleton.java
@@ -2,7 +2,6 @@ package by.itAcademy.homeworks.patterns;
 
 import java.time.LocalDate;
 
-<<<<<<< HEAD
 public class MySingleton {
     private static MySingleton example = new MySingleton();
     private final LocalDate ld = LocalDate.now();
@@ -29,15 +28,6 @@ public static MySingleton getExample(){
 //        }
 //        return example;
 //    }
-=======
-public enum  MySingleton {
-    example;
-
-    private final LocalDate now=LocalDate.now();
-    public LocalDate getNow() {
-        return now;
-    }
->>>>>>> cf6323eb23165e56ff2e7b3b8ca7f1e15b56c1fe
 
     // если объект создается двумя разными потоками одновременно,
     // может возникнуть ситуация, когда они почти одновременно пройдут условие и
