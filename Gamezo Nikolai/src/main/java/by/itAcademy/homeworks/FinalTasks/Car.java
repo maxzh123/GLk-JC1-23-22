1d7cb4e83f20c858ef53aac3f5454eb5b512feaa
gamezo90 <gamezo90@mail.ru>
2022-05-19 14:47:54 +0300

    commit

--- Gamezo Nikolai/src/main/java/by/itAcademy/homeworks/FinalTasks/Car.java
+++ Gamezo Nikolai/src/main/java/by/itAcademy/homeworks/FinalTasks/Car.java
@@ -10,17 +10,39 @@ public class Car {
         MERCEDES,
         BMW
     }
-    public Car(String name, int engineCapacity,CarModel carModel) {
+
+    public Car(String name, int engineCapacity, CarModel carModel) {
         this.name = name;
         this.engineCapacity = engineCapacity;
         this.carModel = carModel;
     }
 
+    public String getName() {
+        return name;
+    }
+
     public int getEngineCapacity() {
         return engineCapacity;
     }
-    public String toString(){
-        return "<" + name + "> : <"  + carModel  + "> : " + engineCapacity + " ";
+
+    public CarModel getCarModel() {
+        return carModel;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setEngineCapacity(int engineCapacity) {
+        this.engineCapacity = engineCapacity;
+    }
+
+    public void setCarModel(CarModel carModel) {
+        this.carModel = carModel;
+    }
+
+    public String toString() {
+        return "<" + name + "> : <" + carModel + "> : " + engineCapacity;
     }
 }
 
